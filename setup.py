df5c7f2ee4ae7f446a4fed783896c6f5122cc064
Anders Logg <logg@simula.no>
2009-11-05 17:49:15 +0100

    Add basic directory structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "CBC.Solve",
+      version = "0.1.0-dev",
+      description = "CBC Solver Collection",
+      author = "Simula Research Laboratory and <authors>",
+      author_email = "logg@simula.no",
+      url = "http://www.fenics.org/wiki/CBC.Solve/",
+      packages = ["cbc",
+                  "cbc.common",
+                  "cbc.beat",
+                  "cbc.flow",
+                  "cbc.twist"],
+      package_dir = {"cbc": "cbc"},
+      scripts = [],
+      data_files = [])
