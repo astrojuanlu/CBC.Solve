f994d33a45d6f2e306d5de9aa566ddd82c715185
Kristoffer Selim <selim@simula.no>
2009-11-11 14:17:03 +0100

    Move some files around

--- sandbox/fsi/fsi.py
+++ sandbox/fsi/fsi.py
@@ -7,14 +7,14 @@ from cbc.twist import *
 # MESH AND BOUNDARY DEFINITION
 #######################################################################################
 
-# Create structure sub domain
-# FIX: Use variables!
+# FIXME: Use variables for width and height etc
+
+# Define structure sub domain
 class Structure(SubDomain):
      def inside(self, x, on_boundary):
           return x[0] >= 1.4 and x[0] <= 1.6 and x[1] <= 0.5
 
 # Create mesh 
-# FIX: Use variables!
 mesh = Rectangle(0.0, 0.0, 3.0, 1.0, 60, 20)
     
 # Create sub domain markers, 0 for fluid, 1 for structure
@@ -24,42 +24,23 @@ structure = Structure()
 structure.mark(sub_domains, 1)
 
 # Extract sub meshes for fluid and structure
-structure_mesh = SubMesh(mesh, sub_domains, 1)
 fluid_mesh = SubMesh(mesh, sub_domains, 0)  
+structure_mesh = SubMesh(mesh, sub_domains, 1)
 
 # Extract matching indices for fluid and structure
 structure_to_fluid = compute_vertex_map(structure_mesh, fluid_mesh)
 
-# Define inflow boundary for the fluid
-class InflowBoundary(SubDomain):
-     def inside(self, x, on_boundary):
-          return on_boundary and x[0] < DOLFIN_EPS
+# Define inflow boundary
+def inflow(x):
+     return x[0] < DOLFIN_EPS
 
-# Define outflow boundary for the fluid
-# FIX: Use variables!
-class OutflowBoundary(SubDomain):
-     def inside(self, x, on_boundary):
-          return on_boundary and x[0] > 3.0 - DOLFIN_EPS
+# Define outflow boundary
+def outflow(x):
+     return x[0] > 3.0 - DOLFIN_EPS
 
-# Define noslip boundary for the fluid
-# FIX: Use variables!
-class NoslipBoundary(SubDomain):
-     def inside(self, x, on_boundary):
-          on_inflow  = x[0] == 0.0 and x[1] > 0.0 and x[1] < 1.0
-          on_outflow = x[0] == 3.0 and x[1] > 0.0 and x[1] < 1.0
-          return on_boundary and not on_inflow and not on_outflow
-
-
-# Create no-slip boundary for the fluid
-noslip_boundary = NoslipBoundary()
-
-# Create inflow-boundary for the fluid
-inflow_boundary = InflowBoundary()
-
-# Create inflow-boundary for the fluid
-outflow_boundary = OutflowBoundary()
-
-
+# Define noslip boundary
+def noslip(x, on_boundary):
+     return on_boundary and not inflow(x) and not outflow(x)
 
 #######################################################################################
 # SOLVERS
@@ -72,40 +53,35 @@ class FluidProblem(NavierStokesProblem):
      def mesh(self):
           return fluid_mesh
 
-     def structure_mesh(self):
-          return self.structure_mesh()
-
      def viscosity(self):
           return 1.0 / 8.0
 
      def end_time(self):
           return 0.5
 
-     def initial_conditions(self, V, Q):
-          u0 = Constant(V.mesh(), (0, 0))
-          p0 = Expression("1 - x[0]", V=Q)
-
-          return u0, p0
-
+     #def initial_conditions(self, V, Q):
+     #     u0 = Constant(V.mesh(), (0, 0))
+     #     p0 = Expression("1 - x[0]", V=Q)
+     #     return u0, p0
 
      def boundary_conditions(self, V, Q):
           
           # Create no-slip boundary condition for velocity
-          bcu = DirichletBC(V, Constant(V.mesh(), (0, 0)), noslip_boundary)
+          bcu = DirichletBC(V, Constant(V.mesh(), (0, 0)), noslip)
+
+          # FIXME: Anders fix DirichletBC to take int or float instead of Constant
 
           # Create inflow and outflow boundary conditions for pressure
-          bcp0 = DirichletBC(Q, Constant(Q.mesh(), 1), inflow_boundary)
-          bcp1 = DirichletBC(Q, Constant(Q.mesh(), 0), outflow_boundary)
+          bcp0 = DirichletBC(Q, Constant(Q.mesh(), 1), inflow)
+          bcp1 = DirichletBC(Q, Constant(Q.mesh(), 0), outflow)
 
           return [bcu], [bcp0, bcp1]
-
     
      def info(self):
           return "Pressure driven channel (2D) with an obstructure"
 
      def time_step(self):
           return 0.05
-
 
 # Define struture solver
 class StructureProblem(StaticHyperelasticityProblem):
@@ -116,12 +92,14 @@ class StructureProblem(StaticHyperelasticityProblem):
 
 
 
-# # Solve fluid problem
-# fluid = FSI_FLUID()
-# fluid.solve()
+fluid = 
 
+# Create fluid solver
+fluid_problem = FluidProblem() 
 
-if __name__ == "__main__":
+# Solve fluid problem
+fluid_problem.solve()
+# Edit the Static NS solver!!!!
 
-    problem = fsi_problems()
-    fsi.solve(problem)
+# Create structure solver
+structure_problem = StructureProblem()
