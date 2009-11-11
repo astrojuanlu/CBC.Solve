ea00a94854f9cfbd2ff709b4e99c37e27e3ce411
Harish Narayanan <harish@simula.no>
2009-11-11 19:28:25 +0100

    The dynamic case now works also

--- cbc/twist/equation_solvers.py
+++ cbc/twist/equation_solvers.py
@@ -57,6 +57,7 @@ class MomentumBalanceSolver(CBCSolver):
         # Get problem parameters
         mesh      = problem.mesh()
         dt        = problem.time_step()
+
         end_time  = problem.end_time()
 
         # Define function spaces
@@ -76,20 +77,56 @@ class MomentumBalanceSolver(CBCSolver):
         # Test and trial functions
         v  = TestFunction(vector)
         u1 = Function(vector)
+        v1 = Function(vector)
+        a1 = Function(vector)
         du = TrialFunction(vector)
         
         # Initial displacement and velocity
         u0 = interpolate(u0, vector)
         v0 = interpolate(v0, vector)
+        v1 = interpolate(v0, vector)
 
-        # Initial acceleration
-        
+        # Parameters pertinent to (HHT) time integration
+        # alpha = 1.0
+        beta = 0.25
+        gamma = 0.5
+    
+        # Determine initial acceleration
+        a0 = TrialFunction(vector)
+        P0 = problem.first_pk_stress(u0)
+        a_accn = inner(a0, v)*dx
+        L_accn = - inner(P0, Grad(v))*dx + inner(B, v)*dx \
+                 + inner(T, v)*ds
+        problem_accn = VariationalProblem(a_accn, L_accn)
+        a0 = problem_accn.solve()
+
+        k = Constant(mesh, dt)
+        a1 = a0*(1.0 - 1.0/(2*beta)) - (u0 - u1 + k*v0)/(beta*k**2)
+
+        # Get reference density
+        rho0 = problem.reference_density(scalar)
+        density_type = str(rho0.__class__)
+        if not ("dolfin" in density_type):
+            print "Converting given density to a DOLFIN Constant"
+            rho0 = Constant(mesh, rho0)
+
         # Piola-Kirchhoff stress tensor based on the material model
         P = problem.first_pk_stress(u1)
 
+#         # FIXME: A general version of the trick below is what should
+#         # be used instead. The commentend-out lines only work well for
+#         # quadratically nonlinear models, e.g. St. Venant Kirchhoff.
+        
+#         # S0 = problem.second_pk_stress(u0)
+#         # S1 = problem.second_pk_stress(u1)
+#         # Sm = 0.5*(S0 + S1)
+#         # Fm = DeformationGradient(0.5*(u0 + u1))
+#         # P  = Fm*Sm
+
         # The variational form corresponding to hyperelasticity
-        L = inner(P, Grad(v))*dx - inner(T, v)*ds \
-            - inner(B, v)*dx 
+        L = int(problem.is_dynamic())*rho0*inner(a1, v)*dx \
+        + inner(P, Grad(v))*dx - inner(T, v)*ds \
+        - inner(B, v)*dx
         a = derivative(L, u1, du)
 
         # Store variables needed for time-stepping
@@ -99,7 +136,15 @@ class MomentumBalanceSolver(CBCSolver):
         self.L = L
         self.bcu = bcu
         self.u0 = u0
+        self.v0 = v0
+        self.a0 = a0
         self.u1 = u1
+        self.v1 = v1
+        self.a1 = a1
+        self.k  = k
+        self.beta = beta
+        self.gamma = gamma
+        self.vector = vector
 
         #FIXME: Figure out why I am needed
         self.mesh = mesh
@@ -114,7 +159,7 @@ class MomentumBalanceSolver(CBCSolver):
         # Time loop
         while self.t <= self.end_time:
 
-#            print "Solving the problem at time t = " + str(self.t)
+            print "Solving the problem at time t = " + str(self.t)
             self.step(self.dt)
             self.update()
 
@@ -126,68 +171,16 @@ class MomentumBalanceSolver(CBCSolver):
 
     def update(self):
         """Update problem at time t"""
+
+        a1 = self.a0*(1.0 - 1.0/(2*self.beta)) - (self.u0 - self.u1 + self.k*self.v0)/(self.beta*self.k**2)
+        self.a1 = project(a1, self.vector)
+        v1 = self.v0 + self.k*((1 - self.gamma)*self.a1 + self.gamma*self.a0)
+        self.v1 = project(v1, self.vector)
+
         self.t = self.t + self.dt
+
         self.u0.assign(self.u1)
+        self.v0.assign(self.v1)
+        self.a0.assign(self.a1)
+
         plot(self.u0, title="Displacement", rescale=True)
-
-        
-
-        # v1 = Function(vector)
-        # a1 = Function(vector)
-
-            # Update
-#             a1 = a0*(1.0 - 1.0/(2*beta)) - (u0 - u1 + k*v0)/(beta*k**2)
-#             a1 = project(a1, vector)
-#             v1 = v0 + k*((1 - gamma)*a1 + gamma*a0)
-#             v1 = project(v1, vector)
-
-#             v0.assign(v1)
-#             a0.assign(a1)
-
-
-#         # Get reference density
-#         rho0 = problem.reference_density(scalar)
-#         density_type = str(rho0.__class__)
-#         if not ("dolfin" in density_type):
-#             print "Converting given density to a DOLFIN Constant"
-#             rho0 = Constant(mesh, rho0)
-
-
-#         # Determine initial acceleration
-#         a0 = TrialFunction(vector)
-#         P = problem.first_pk_stress(u0)
-#         a_accn = inner(a0, v)*dx
-#         L_accn = - inner(P, Grad(v))*dx + inner(B, v)*dx \
-#                  + inner(T, v)*ds
-#         problem_accn = VariationalProblem(a_accn, L_accn)
-#         a0 = problem_accn.solve()
-
-#         # Time step size
-#         k  = Constant(mesh, dt)
-        
-#         # FIXME: A general version of the trick below is what should
-#         # be used instead. The commentend-out lines only work well for
-#         # quadratically nonlinear models, e.g. St. Venant Kirchhoff.
-        
-#         # S0 = problem.second_pk_stress(u0)
-#         # S1 = problem.second_pk_stress(u1)
-#         # Sm = 0.5*(S0 + S1)
-#         # Fm = DeformationGradient(0.5*(u0 + u1))
-#         # P  = Fm*Sm
-
-#         # Parameters pertinent to (HHT) time integration
-#         # alpha = 1.0
-#         beta = 0.25
-#         gamma = 0.5
-
-#         a1 = a0*(1.0 - 1.0/(2*beta)) - (u0 - u1 + k*v0)/(beta*k**2)
-
-
-#         L = int(problem.is_dynamic())*rho0*inner(a1, v)*dx \
-#         + inner(P, Grad(v))*dx - inner(T, v)*ds \
-#         - inner(B, v)*dx
-
-
-
-
-
