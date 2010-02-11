2328bbba387261a72f827e1d200c068d412b25a3
selim <selim@selim-laptop>
2010-02-11 09:44:59 +0100

    Added the dual forms

--- sandbox/fsi/dual.py
+++ sandbox/fsi/dual.py
@@ -2,6 +2,8 @@
 # As a first try we solve a stationary problem
 
 from common import *
+from cbc.common import CBCSolver
+from cbc.twist.kinematics import SecondOrderIdentity
 from numpy import array, append, zeros
 
 # Define function spaces defined on the whole domain
@@ -72,7 +74,7 @@ U_S_global_dofs = append(S_global_index, S_global_index + Nv)
 U_M_global_dofs = append(M_global_index, M_global_index + Nv)
 
 # Get rid of P2 dofs for u_F and create a P1 function
-u_F_subdofs = append(u_F_subdofs[:Nv_F], u_F_subdofs[Nv_F + Ne_F: 2*Nv_F +Ne_F])
+u_F_subdofs = append(u_F_subdofs[:Nv_F], u_F_subdofs[Nv_F + Ne_F: 2*Nv_F + Ne_F])
 
 # Transfer the stored primal solutions on the dual mesh Omega
 U_F.vector()[U_F_global_dofs] = u_F_subdofs
@@ -81,12 +83,8 @@ U_S.vector()[U_S_global_dofs] = U_S_subdofs
 U_M.vector()[U_M_global_dofs] = U_M_subdofs
 
 
-#plot(U_F, title="DUAL U_F", interactive=True)
-plot(U_F, title="DUAL U_S", interactive=True)
-
-
-
-
+# plot(U_F, title="DUAL U_F", interactive=True)
+# plot(U_F, title="DUAL U_S", interactive=True)
 
 
 file = File("U_F_dual.pvd")
@@ -118,140 +116,145 @@ file << U_S
 # (using bc.zero on the off diagonal terms)
 
 
+# # Second order identity tensor
+# def SecondOrderIdentity(u):
+#     return variable(Identity(u.cell().d))
 
-# # Define the Jacobian matrices and determinants
-# def F(u):
-#     I = SecondOrderIdentity(u)
-#     F = (I + grad(u)) 
-#     return F
+# Define the Jacobian matrices and determinants
+def F(u):
+    I = SecondOrderIdentity(u)
+    F = (I + grad(u)) 
+    return F
 
-# def F_inv(u):
-#     I = SecondOrderIdentity(u)
-#     F_inv  = inv((I + grad(u)))
-#     return F_inv
+def F_inv(u):
+    I = SecondOrderIdentity(u)
+    F_inv  = inv((I + grad(u)))
+    return F_inv
 
-# def F_T(u):
-#     I = SecondOrderIdentity(u)
-#     F_T  = ((I + grad(u))).T
-#     return F_T
+def F_T(u):
+    I = SecondOrderIdentity(u)
+    F_T  = ((I + grad(u))).T
+    return F_T
 
-# def F_invT(u):
-#     I = SecondOrderIdentity(u)
-#     F_invT  = (inv((I + grad(u)))).T
-#     return F_invT
+def F_invT(u):
+    I = SecondOrderIdentity(u)
+    F_invT  = (inv((I + grad(u)))).T
+    return F_invT
 
-# def J(u):
-#     I = SecondOrderIdentity(u)
-#     J = det(I + grad(u))
-#     return J
+def J(u):
+    I = SecondOrderIdentity(u)
+    J = det(I + grad(u))
+    return J
 
-# # DJ(u,w) is J(u) linearized around w (w = test function)
-# def DJ(u,w):
-#     DJ = w[0].dx(0)*(1 - u[1].dx(1)) - w[0].dx(1)*u[1].dx(0) \
-#         -w[1].dx(0)*u[0].dx(1) + w[1].dx(1)*(1 + u[0].dx(0))
-#     return DJ
+# DJ(u,w) is J(u) linearized around w (w = test function)
+def DJ(u,w):
+    DJ = w[0].dx(0)*(1 - u[1].dx(1)) - w[0].dx(1)*u[1].dx(0) \
+        -w[1].dx(0)*u[0].dx(1) + w[1].dx(1)*(1 + u[0].dx(0))
+    return DJ
 
-# def I(u):
-#     I = SecondOrderIdentity(u)
-#     return I
+def I(u):
+    I = SecondOrderIdentity(u)
+    return I
 
-# def sym_gradient(u):
-#     sym_gradient = 0.5*(grad(u)+ grad(u).T)
-#     return sym_gradient
+def sym_gradient(u):
+    sym_gradient = 0.5*(grad(u)+ grad(u).T)
+    return sym_gradient
     
+# Define constants
+# FIXME: should not be defined here!
+mu_F = 1
+mu_S = 1
+lamb_S = 1
+mu_M = 1 
+lamb_M = 1
 
-# # Define constants
-# mu_F = 1
-# mu_S = 1
-# lamb_S = 1
-# mu_M = 1 
-# lamb_M = 1
+# Fluid eq. linearized around fluid variables
+A_FF01 = 0 # time--dependent
+A_FF02 = 0 # time--dependent
+A_FF03 =  inner(Z_UF, dot(grad(U_F) , dot(F(U_M), v_F)))*dx(0)
+A_FF04 =  inner(grad(Z_UF), mu_F*dot(grad(v_F) , dot(F_inv(U_M), F_invT(U_M))))*dx(0)
+A_FF05 =  inner(grad(Z_UF), mu_F*dot(F_invT(U_M) , dot(grad(v_F).T, F_invT(U_M))))*dx(0)
+#A_FF06 =  -inner(grad(Z_UF), (q_F*dot(I(U_M), F_invT(U_M))))*dx # FIXME: Which one should we use?
+A_FF06 =  -inner(grad(Z_UF), q_F*F_invT(U_M))*dx(0)
+A_FF07 =  inner(Z_PF, inner(F_invT(U_M), grad(v_F)))*dx(0)
 
-# # Fluid eq. linearized around fluid variables
-# A_FF01 = 0 # time--dependent
-# A_FF02 = 0 # time--dependent
-# A_FF03 =  inner(Z_UF, dot(grad(U_F) , dot(F(U_M), v_F)))*dx
-# A_FF04 =  inner(grad(Z_UF), mu_F*dot(grad(v_F) , dot(F_inv(U_M), F_invT(U_M))))*dx
-# A_FF05 =  inner(grad(Z_UF), mu_F*dot(F_invT(U_M) , dot(grad(v_F).T, F_invT(U_M))))*dx
-# #A_FF06 =  -inner(grad(Z_UF), (q_F*dot(I(U_M), F_invT(U_M))))*dx # FIXME: Which one should we use?
-# A_FF06 =  -inner(grad(Z_UF), q_F*F_invT(U_M))*dx
-# A_FF07 =  inner(Z_PF, inner(F_invT(U_M), grad(v_F)))*dx
+# Collect A_FF form
+A_FF_sum = A_FF03 + A_FF04 + A_FF05 + A_FF06 + A_FF07
 
-# # Collect A_FF form
-# A_FF_sum = A_FF03 + A_FF04 + A_FF05 + A_FF06 + A_FF07
+# Fluid eq. linearized around structure 
+A_FS = 0 # by def. of the problem
 
-# # Fluid eq. linearized around structure 
-# A_FS = 0 # by def. of the problem
+# Fluid eq. linearized around mesh variable
+A_FM01 = 0 # time--dependent
+A_FM02 = 0 # time--dependent
+A_FM03 = -inner(grad(Z_UF), dot(mu_F*(dot(grad(U_F), dot(F_inv(U_M), dot(grad(v_M),F_inv(U_M))))),F_invT(U_M)))*dx(0)
+A_FM04 = -inner(grad(Z_UF), dot(mu_F*(dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M), grad(U_F).T )))),F_invT(U_M)))*dx(0)
+A_FM05 = -inner(grad(Z_UF), dot(mu_F*(dot(grad(U_F), dot(F_inv(U_M), dot(F_invT(U_M), grad(v_M).T )))),F_invT(U_M)))*dx(0)
+A_FM06 = -inner(grad(Z_UF), dot(mu_F*(dot(F_invT(U_M) , dot(grad(U_M).T, dot(F_invT(U_M), grad(v_M).T )))),F_invT(U_M)))*dx(0)
+A_FM07 =  inner(grad(Z_UF), dot(dot( P_F*I(U_F),F_invT(U_M)) ,  dot(grad(v_M).T ,F_invT(U_M) )))*dx(0)
+A_FM08 = -inner(Z_PF, inner( dot(F_invT(U_M), grad(v_M).T)  , dot( I(U_M), grad(U_F))))*dx(0) # FIXME: Is this the right way to write it????
 
-# # Fluid eq. linearized around mesh variable
-# A_FM01 = 0 # time--dependent
-# A_FM02 = 0 # time--dependent
-# A_FM03 = -inner(grad(Z_UF), dot(mu_F*(dot(grad(U_F), dot(F_inv(U_M), dot(grad(v_M),F_inv(U_M))))),F_invT(U_M)))*dx
-# A_FM04 = -inner(grad(Z_UF), dot(mu_F*(dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M), grad(U_F).T )))),F_invT(U_M)))*dx
-# A_FM05 = -inner(grad(Z_UF), dot(mu_F*(dot(grad(U_F), dot(F_inv(U_M), dot(F_invT(U_M), grad(v_M).T )))),F_invT(U_M)))*dx
-# A_FM06 = -inner(grad(Z_UF), dot(mu_F*(dot(F_invT(U_M) , dot(grad(U_M).T, dot(F_invT(U_M), grad(v_M).T )))),F_invT(U_M)))*dx
-# A_FM07 =  inner(grad(Z_UF), dot(dot( P_F*I(U_F),F_invT(U_M)) ,  dot(grad(v_M).T ,F_invT(U_M) )))*dx
+# Collect A_FM form
+A_FM_sum = A_FM03 + A_FM04 + A_FM05 + A_FM06 + A_FM07 + A_FM08
 
-# # Collect A_FM form
-# A_FM_sum = A_FM03 + A_FM04 + A_FM05 + A_FM06 + A_FM07
+# Define FSI normal
+N_S = FacetNormal(Omega_S)
 
-# # FIXME: A_FM08
-# #A_FM08 = -inner(Z_PF, dot( dot(F_invT(U_M), grad(v_M).T)  , dot( I(U_M), grad(U_F))))*dx 
-# #A_FM08 = -inner(Z_PF , (inner(dot(F_invT*(U_M), dot(grad(v_M).T, F_invT(U_M)))), grad(U_F)))*dx
-# #A_FM08 = inner(F_invT(U_M),grad(v_M))*dx
+# Structure eq. linearized around the fluid variables
+A_SF01 = -inner(Z_US, mu_F*J(U_M)*dot(dot(grad(v_M), F_inv(U_M)), dot(F_invT(U_M), N_S)))*ds(1)
+A_SF02 = -inner(Z_US, mu_F*J(U_M)*dot(dot(F_invT(U_M), grad(v_M).T), dot(F_invT(U_M), N_S)))*ds(1)
+A_SF03 =  inner(Z_US, mu_F*J(U_M)*q_F*dot(I(U_M), dot(F_invT(U_M), N_S)))*ds(1)
 
-# # Structure eq. linearized around the fluid variables
-# A_SF01 = -inner(Z_US, mu_F*J(U_M)*dot(dot(grad(v_M), F_inv(U_M)), dot(F_invT(U_M), N_S)))*ds
-# A_SF02 = -inner(Z_US, mu_F*J(U_M)*dot(dot(F_invT(U_M), grad(v_M).T), dot(F_invT(U_M), N_S)))*ds
-# A_SF03 =  inner(Z_US, mu_F*J(U_M)*q_F*dot(I(U_M), dot(F_invT(U_M), N_S)))*ds
+# Collect A_SF form
+A_SF_sum = A_SF01 + A_SF02 + A_SF03 
 
-# # Collect A_SF form
-# A_SF_sum = A_SF01 + A_SF02 + A_SF03 
+# Structure eq. linearized around the structure variable
+A_SS01 = 0 # time--dependent
+A_SS02 = inner(grad(Z_US), mu_S*dot(grad(v_S), F_T(U_S)))*dx(1)
+A_SS03 = inner(grad(Z_US), mu_S*dot(F(U_S), grad(v_S).T))*dx(1)
+A_SS04 = inner(grad(Z_US), 0.5*lamb_S*tr(dot( grad(v_S), F_T(U_S)))*I(U_S))*dx(1)
+A_SS05 = inner(grad(Z_US), 0.5*lamb_S*tr(dot(F(U_S), grad(v_S)))*I(U_S))*dx(1)
 
-# # Structure eq. linearized around the structure variable
-# A_SS01 = 0 # time--dependent
-# A_SS02 = inner(grad(Z_US), mu_S*dot(grad(v_S), F_T(U_S)))*dx
-# A_SS03 = inner(grad(Z_US), mu_S*dot(F(U_S), grad(v_S).T))*dx
-# A_SS04 = inner(grad(Z_US), 0.5*lamb_S*tr(dot( grad(v_S), F_T(U_S)))*I(U_S))*dx
-# A_SS05 = inner(grad(Z_US), 0.5*lamb_S*tr(dot(F(U_S), grad(v_S)))*I(U_S))*dx
+# Collect A_SS form
+A_SS_sum = A_SS02 + A_SS02 + A_SS04 + A_SS05
 
-# # Collect A_SS form
-# A_SS_sum = A_SS02 + A_SS02 + A_SS04 + A_SS05
+# Structure eq. linearized around mesh variable
+A_SM01 = -inner(Z_US, DJ(U_M,v_M)*mu_F*dot(dot(grad(U_F), F_inv(U_F)), dot(F_invT(U_M), N_S)))*ds(1)
+A_SM02 = -inner(Z_US, DJ(U_M,v_M)*mu_F*dot(dot(F_invT(U_F), grad(U_F).T), dot(F_invT(U_M), N_S)))*ds(1)
+A_SM03 =  inner(Z_US, DJ(U_M,v_M)*dot(P_F*I(U_F), dot(F_invT(U_M),N_S)))*ds(1)
+A_SM04 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F), dot(F_inv(U_M),grad(v_M))), dot(F_inv(U_M), dot(F_invT(U_M), N_S))))*ds(1) 
+A_SM05 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F).T, dot(F_invT(U_M), grad(v_M).T)), dot(F_invT(U_M), dot(F_invT(U_M),N_S))))*ds(1)
+A_SM06 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F),F_inv(U_M)),dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M),N_S)))))*ds(1)
+A_SM07 =  inner(Z_US, J(U_M)*mu_F*dot(dot(F_invT(U_M),grad(U_M).T),dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M),N_S)))))*ds(1)
+A_SM08 = -inner(Z_US, J(U_M)*dot(dot(P_F*I(U_F),F_invT(U_M)), dot(grad(v_M).T, dot(F_invT(U_M), N_S))))*ds(1)
 
-# # Structure eq. linearized around mesh variable
-# A_SM01 = -inner(Z_US, DJ(U_M,v_M)*mu_F*dot(dot(grad(U_F), F_inv(U_F)), dot(F_invT(U_M), N_S)))*ds
-# A_SM02 = -inner(Z_US, DJ(U_M,v_M)*mu_F*dot(dot(F_invT(U_F), grad(U_F).T), dot(F_invT(U_M), N_S)))*ds
-# A_SM03 =  inner(Z_US, DJ(U_M,v_M)*dot(P_F*I(U_F), dot(F_invT(U_M),N_S)))*ds
-# A_SM04 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F), dot(F_inv(U_M),grad(v_M))), dot(F_inv(U_M), dot(F_invT(U_M), N_S))))*ds 
-# A_SM05 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F).T, dot(F_invT(U_M), grad(v_M).T)), dot(F_invT(U_M), dot(F_invT(U_M),N_S))))*ds
-# A_SM06 =  inner(Z_US, J(U_M)*mu_F*dot(dot(grad(U_F),F_inv(U_M)),dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M),N_S)))))*ds
-# A_SM07 =  inner(Z_US, J(U_M)*mu_F*dot(dot(F_invT(U_M),grad(U_M).T),dot(F_invT(U_M), dot(grad(v_M).T, dot(F_invT(U_M),N_S)))))*ds
-# A_SM08 = -inner(Z_US, J(U_M)*dot(dot(P_F*I(U_F),F_invT(U_M)), dot(grad(v_M).T, dot(F_invT(U_M), N_S))))*ds 
+# Collect A_SM form
+A_SM_sum = A_SM01 + A_SM02 + A_SM03 + A_SM04 + A_SM05 + A_SM06 + A_SM07 + A_SM08 
 
-# # FIXME: Pick out the FSI contribution
-# # Do as we did for the fluid stress!!!
 
-# # Collect A_SM form
-# A_SM_sum = A_SM01 + A_SM02 + A_SM03 + A_SM04 + A_SM05 + A_SM06 + A_SM07 + A_SM08 
+# Mesh eq. linearized around fluid variables 
+A_MF = 0 # by def. of the problem
 
-# # Mesh eq. linearized around fluid variables 
-# A_MF = 0 # by def. of the problem
+# Mesh eq. linearized around structure variable
+# FIXME: Should not be zero with an extension operator
+A_MS = 0
 
-# # Mesh eq. linearized around structure variable
-# # FIXME: Should not be zero with an extension operator
-# A_MS = 0
+# Mesh eq. linearized around mesh variable
+A_MM_sum = inner(sym_gradient(v_M),(2*mu_M*sym_gradient(U_M) + lamb_M*grad(U_M)))*dx(0)
 
-# # Mesh eq. linearized around mesh variable
-# # FIXME: Add mesh eq.
-# #A_MM = inner(
+# Assemble the non-zero blocks
+A_FF = assemble(A_FF_sum)
+A_SF = assemble(A_SF_sum)
+A_SS = assemble(A_SS_sum)
+A_FM = assemble(A_FM_sum)
+A_SM = assemble(A_SM_sum)
+A_MM = assemble(A_MM_sum)
 
-# # Assemble the non-zero blocks
-# A_FF = assemble(A_FF_sum)
-# # A_SF = assemble(A_SF_sum)
-# # A_SS = assemble(A_SS_sum)
-# # A_FM = assemble(A_FM_sum)
-# # A_SM = assemble(A_SM_sum)
 
 # # Create block matrix for dual system 
 # dual_sys = BlockMatrix(3, 3)
 # dual_sys[0,0] = A_FF
+
+# print "I was here"
+# test = assemble(A_MM_sum)
+# print "and here to"
