46643c0f108b4b487b69f4e1b3ae2835eb766b4a
Harish Narayanan <hnarayanan@gmail.com>
2010-03-22 14:10:33 +0100

    Added code to analytically compute eigenvalues of 2x2 and 3x3 matrices so strain energies can be posed in terms of principal stretches

--- cbc/twist/kinematics.py
+++ cbc/twist/kinematics.py
@@ -82,6 +82,28 @@ def IsochoricCauchyGreenInvariants(u):
     [I1bar, I2bar, I3bar] = Invariants(Cbar)
     return [variable(I1bar), variable(I2bar)]
 
+# Principal stretches
+def PrincipalStretches(u):
+    C = RightCauchyGreen(u)
+    if (u.cell().d == 2):
+        D = sqrt(tr(C)*tr(C) - 4.0*det(C))
+	eig1 = sqrt(0.5*(tr(C) + D))
+	eig2 = sqrt(0.5*(tr(C) - D))
+	return [variable(eig1), variable(eig2)]
+    if (u.cell().d == 3):
+	c = (1.0/3.0)*tr(C)
+	D = C - c*SecondOrderIdentity(u)
+	q = (1.0/2.0)*det(D)
+	p = (1.0/6.0)*inner(D, D)
+	phi = (1.0/3.0)*atan(sqrt(p**3.0 - q**2.0)/q)
+	if (phi < 0.0):
+            phi = phi + DOLFIN_PI/3.0
+	end
+	eig1 = sqrt(c + 2*sqrt(p)*cos(phi))
+	eig2 = sqrt(c - sqrt(p)*(cos(phi) + sqrt(3)*sin(phi)))
+        eig3 = sqrt(c - sqrt(p)*(cos(phi) - sqrt(3)*sin(phi)))
+        return [variable(eig1), variable(eig2), variable(eig3)]
+
 # Pull-back of a two-tensor from the current to the reference
 # configuration
 def PiolaTransform(A, u):
