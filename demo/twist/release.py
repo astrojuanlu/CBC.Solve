aedc5bbcdfe8e9bb6e1790d0fa60057c2360a231
Harish Narayanan <hnarayanan@gmail.com>
2010-04-18 23:37:46 +0200

    Fix all twist demos

--- demo/twist/release.py
+++ demo/twist/release.py
@@ -3,7 +3,6 @@ __copyright__ = "Copyright (C) 2009 Simula Research Laboratory and %s" % __autho
 __license__  = "GNU GPL Version 3 or any later version"
 
 from cbc.twist import *
-from numpy import array, loadtxt
 
 class Release(Hyperelasticity):
 
@@ -20,19 +19,18 @@ class Release(Hyperelasticity):
     def is_dynamic(self):
         return True
 
-    def reference_density(self, scalar):
+    def reference_density(self):
         return 1.0
 
-    def initial_conditions(self, vector):
+    def initial_conditions(self):
         """Return initial conditions for displacement field, u0, and
-        velocity field, v0""" 
-        u0 = Function(vector)
-        u0.vector()[:] = loadtxt("twisty.txt")[:]
-        v0 = Expression(("0.0", "0.0", "0.0"), V = vector)
+        velocity field, v0"""
+        u0 = "twisty.txt"
+        v0 = Expression(("0.0", "0.0", "0.0"))
         return u0, v0
 
-    def dirichlet_conditions(self, vector):
-        clamp = Expression(("0.0", "0.0", "0.0"), V = vector)
+    def dirichlet_conditions(self):
+        clamp = Expression(("0.0", "0.0", "0.0"))
         return [clamp]
 
     def dirichlet_boundaries(self):
