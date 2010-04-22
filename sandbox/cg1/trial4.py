bf30461d6e200f7f71454e8263f4e8046a808fd1
Harish Narayanan <hnarayanan@gmail.com>
2010-04-22 14:31:31 +0200

    Added a simple 2d linear elastic case to the sandbox to check the energy conservation of the time integration scheme

---
+++ sandbox/cg1/trial4.py
@@ -0,0 +1,77 @@
+from dolfin import *
+from numpy import array, loadtxt
+
+parameters["form_compiler"]["cpp_optimize"] = True
+parameters["form_compiler"]["optimize"] = True
+
+mesh = UnitSquare(32, 32)
+vector = VectorFunctionSpace(mesh, "CG", 1)
+
+B  = Expression(("0.0", "0.0"))
+T  = Expression(("0.0", "0.0"))
+
+mixed_element = MixedFunctionSpace([vector, vector])
+V = TestFunction(mixed_element)
+dU = TrialFunction(mixed_element)
+U = Function(mixed_element)
+U0 = Function(mixed_element)
+
+xi, eta = split(V)
+u, v = split(U)
+u0, v0 = split(U0)
+
+# Load initial displacement from file
+_u0 = loadtxt("u0.txt")[:]
+U0.vector()[0:len(_u0)] = _u0[:]
+
+u_mid = 0.5*(u0 + u)
+v_mid = 0.5*(v0 + v)
+
+mu    = Constant(3.85)
+lmbda = Constant(5.77)
+
+rho0 = Constant(1.0)
+dt = Constant(0.01)
+
+def PE(u):
+    return assemble(inner(epsilon(u), sigma(u))*dx, mesh=mesh)
+
+def KE(v):
+    return assemble(0.5*rho0*inner(v, v)*dx, mesh=mesh)
+
+def TE(u, v):
+    return PE(u) + KE(v)
+
+def epsilon(v):
+    return sym(grad(v))
+
+def sigma(v):
+    return 2.0*mu*epsilon(v) + lmbda*tr(epsilon(v))*Identity(v.cell().d)
+
+L = rho0*inner(v - v0, xi)*dx + dt*inner(sigma(u_mid), sym(grad(xi)))*dx \
+    - dt*inner(B, xi)*dx - dt*inner(T, xi)*ds \
+    + inner(u - u0, eta)*dx - dt*inner(v_mid, eta)*dx 
+a = derivative(L, U, dU)
+
+t = 0.0
+T = 2.0
+
+problem = VariationalProblem(a, L, nonlinear = True)
+
+file = File("displacement.pvd")
+u0, v0 = U0.split(True)
+
+print "Total Energy", TE(u0, v0)
+file << u0
+
+while t < T:
+
+    t = t + float(dt)
+
+    problem.solve(U)
+    u, v = U.split()
+
+    print "Total Energy", TE(u, v)
+    file << u
+
+    U0.assign(U)
