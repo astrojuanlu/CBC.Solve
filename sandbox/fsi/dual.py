e1b41253ff0f10b4b0d9cd9903febd88a84123cb
selim <selim@selim-laptop>
2010-06-13 13:30:09 +0200

    Moved def. of the operators to operators.py

--- sandbox/fsi/dual.py
+++ sandbox/fsi/dual.py
@@ -1,7 +1,7 @@
 from common import *
 from cbc.common import CBCSolver
 from cbc.common.utils import *
-from cbc.twist.kinematics import SecondOrderIdentity
+from operators import *
 from numpy import array, append, zeros, linspace
 
 # Optimize form compiler
@@ -9,15 +9,6 @@ parameters["form_compiler"]["cpp_optimize"] = True
 
 # Plot solution during run-time 
 plot_solution = False
-
-# Define constants FIXME: should be retrived from problem!!!
-rho_F = 1.0
-mu_F = 0.002
-rho_S = 1.0
-mu_S =  0.15
-lmbda_S =  0.25
-mu_M =  3.8461
-lmbda_M =  5.76
 
 # Define function spaces defined on the whole domain
 V_F1 = VectorFunctionSpace(Omega, "CG", 1)
@@ -123,57 +114,8 @@ U_F0  = Function(V_F1)
 U_S0  = Function(V_S)
 P_S0  = Function(Q_S)
 
-# Define operators that is needed in the forms
-def F(u):
-    I = SecondOrderIdentity(u)
-    F = (I + grad(u))
-    return F
-
-def F_inv(u):
-    I = SecondOrderIdentity(u)
-    F_inv  = inv((I + grad(u)))
-    return F_inv
-
-def F_T(u):
-    I = SecondOrderIdentity(u)
-    F_T  = ((I + grad(u))).T
-    return F_T
-
-def F_invT(u):
-    I = SecondOrderIdentity(u)
-    F_invT  = (inv((I + grad(u)))).T
-    return F_invT
-
-def J(u):
-    I = SecondOrderIdentity(u)
-    J = det(I + grad(u))
-    return J
-
-# DJ(u,w) is J(u) linearized around w (w = test function)
-# (u is always U_M)
-def DJ(u,w):
-    DJ = w[0].dx(0)*(1 - u[1].dx(1)) - w[0].dx(1)*u[1].dx(0) \
-        -w[1].dx(0)*u[0].dx(1) + w[1].dx(1)*(1 + u[0].dx(0))
-    return DJ
-
-def J(u):
-    return det(F(u))
-
-I = variable(Identity(2))
-
-def sym_gradient(u):
-    sym_gradient = 0.5*(grad(u)+ grad(u).T)
-    return sym_gradient
-
-def Sigma_F(u,p,v):
-    return  mu_F*(grad(u)*F_inv(v) + F_invT(v)*grad(u).T) - p*I
-
-def sigma_M(u):
-    return 2.0*mu_M*sym_gradient(u) + lmbda_M*tr(sym_gradient(u))*I
-
-
 # Fix time step if needed. Note that this has to be done
-# in oder to save the primal data at the correct time
+# in oder to retrieve primal data at the correct time
 dt, t_range = timestep_range(T, dt) #FIXME: Only the default values in common.py are set!!!
 kn = Constant(dt)
 
