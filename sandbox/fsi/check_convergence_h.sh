49de78fade3b64367e652d37c88077b2f80d57c2
Anders Logg <logg@simula.no>
2010-06-14 20:02:42 +0200

    Bug fix in h-convergence script

--- sandbox/fsi/check_convergence_h.sh
+++ sandbox/fsi/check_convergence_h.sh
@@ -2,5 +2,5 @@ nxs="40 60 80 100 120 140 160 180 200 220 240 260 280 300 320"
 
 for nx in $nxs; do
     echo "Solving with nx = $nx..."
-    python primal.py dt=$dt | grep Functional
+    python primal.py nx=$nx | grep Functional
 done
