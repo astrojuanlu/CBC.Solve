3eb0973458ccbc725e970063c86a50d906ebc120
selim <selim@selim-laptop>
2010-07-15 17:58:38 +0200

    Added info Last changed....

--- sandbox/fsi/primalsolver.py
+++ sandbox/fsi/primalsolver.py
@@ -4,7 +4,7 @@ __author__ = "Kristoffer Selim and Anders Logg"
 __copyright__ = "Copyright (C) 2010 Simula Research Laboratory and %s" % __author__
 __license__  = "GNU GPL Version 3 or any later version"
 
-# Last changed: 2010-06-30
+# Last changed: 2010-07-15
 
 from dolfin import *
 from subproblems import *
