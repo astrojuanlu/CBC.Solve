5f1440dbf6dcdb15686b3853ddd5304903c56edb
selim <selim@selim-laptop>
2010-08-18 23:26:06 +0200

    Removed DJ() to tr(dot(grad(), F). This is the definition of it and this is how its presented in the paper

--- sandbox/fsi/operators.py
+++ sandbox/fsi/operators.py
@@ -19,11 +19,6 @@ def J(v):
     "Return determinant of deformation gradient"
     return det(F(v))
 
-def DJ(v, dv):
-    "Return derivative J'(v) dv of determinant"
-    return dv[0].dx(0)*(1 - v[1].dx(1)) - dv[0].dx(1)*v[1].dx(0) \
-          -dv[1].dx(0)*v[0].dx(1) + dv[1].dx(1)*(1 + v[0].dx(0))
-
 def Sigma_F(U_F, P_F, U_M, mu_F):
     "Return fluid stress in reference domain"
     return mu_F*(grad(U_F)*inv(F(U_M)) + inv(F(U_M)).T*grad(U_F).T) - P_F*I
