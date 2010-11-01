5be360c6e0193ec734251d4237f04643bd715995
selim <selim@selim-laptop>
2010-11-01 13:33:50 +0100

    More work on the script for running problems on BB

--- sandbox/fsi/run_bb.py
+++ sandbox/fsi/run_bb.py
@@ -4,40 +4,38 @@ os.environ['PYTHONPATH'] = "..:../..:" + os.environ['PYTHONPATH']
 days = 20
 
 
-# # # Parameters
-# nx = [80,120,160,200,240,280,320,360,400,440,480,520,560,600,640]
-# dt = [0.0141421356237,0.0094280904158,0.00707106781185,0.00565685424946,0.00471404520786,0.00404061017818,0.0035355339059,0.00314269680521,0.0028284271247,0.00257129738607,0.00235702260386,0.00217571317278,0.00202030508904,0.00188561808308,0.00176776695285]
-# T  = [1.0]
-# smooth = [50]
+# Define problem names
+problem_names = ["channel_with_flap", "driven_cavity_free_bottom", "driven_cavity_fixed_bottom" , "leaky_cavity_free_bottom", "leaky_cavity_fixed_bottom"]
 
-# # Loop jobs                                                                                                                                                                 
-# for i in range(len(nx)):
-#    # for j in range(len(dt)):
-#     for k in range(len(T)):
-#         for l in range(len(smooth)):
-#             jobs =(("python sandbox/fsi/primal.py" + " " + "nx="+ str(nx[i]) + " " + "dt=" + str(dt[i]) + " " +  "T=" + str(T[k]) + " " + "smooth="+ str(smooth[l])))
-#             submit(jobs, nodes=1, ppn=8,  keep_environment=True, walltime=24*days, dryrun=False)
-#             sleep(10)
-
-
-# Define paramters
-w_h = [0.5]
-w_k = [0.4]
-w_c = [0.1]
-d_f = [0.85] # Dorfler fraction
-m_a = [1.0]  # Mesh constant 
-ny  = [20]   
-dt  = [0.02]
-T   = [0.5]
-
-
+#-----------------------------------------------
+# problem_names[0] = channel_with_flap
+# problem_names[1] = driven_cavity_free_bottom
+# problem_names[2] = driven_cavity_fixed_bottom
+# problem_names[3] = leaky_cavity_free_bottom
+# problem_names[4] = leaky_cavity_fixed_bottom
+#-----------------------------------------------
 
 # Define problem
-problem = [driven_cavity_free_bottom]
+problem = problem_names[1]
 
+# Define TOL parameters
+w_h = 0.5
+w_k = 0.4
+w_c = 0.1
 
+# Define mesh parameters
+d_f = 0.65  # Dorfler fraction
+m_a = 1.0   # Mesh constant 
+ny  = 20   
 
-jobs =("python" + str(problem[0])".py" + " " + "--w_h" + str(w_h))
+# Define time parameters
+dt  = 0.04
+T   = 0.1
 
+# Define and submit job 
+jobs =("python"+" "+ str(problem)+".py"+" "+"--w_h"+" "+ str(w_h)+" "+"--w_k"+" "+ str(w_k) +" "+"--w_c"+" "+ str(w_c) +" "+"--T"+" "+ str(T)  +" "+"--dt"+" "+ str(dt)  +" "+"--dorfler_fraction"+" "+ str(d_f) +" "+"--mesh_alpha"+" "+ str(m_a) )
 submit(jobs, nodes=1, ppn=8,  keep_environment=True, walltime=24*days, dryrun=False)
 
+
+ 
+# +" "+"--T"+" "+ str(T)
