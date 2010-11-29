b36e04d9ab6eb83a049cd3544944eaac6e079b78
selim <selim@selim-laptop>
2010-11-29 19:01:12 +0100

    Added solver options.

--- sandbox/fsi/paper1.py
+++ sandbox/fsi/paper1.py
@@ -26,23 +26,23 @@ from fsiproblem import *
 
 # Create application parameters set
 application_parameters = Parameters("application_parameters")
+application_parameters.add("T", 2.5)
+application_parameters.add("dt", 0.05)
 application_parameters.add("ny", 30)
-application_parameters.add("dt", 0.02)
-application_parameters.add("T", 1.0)
-application_parameters.add("w_h", 0.00005) 
-application_parameters.add("w_k", 0.85)
-application_parameters.add("w_c", 0.3)
 application_parameters.add("TOL", 0.5)
-application_parameters.add("mesh_alpha", 1.0)
+application_parameters.add("w_h", 0.1) 
+application_parameters.add("w_k", 0.85)
+application_parameters.add("w_c", 0.05)
 application_parameters.add("dorfler_fraction", 0.2)
+application_parameters.add("mesh_alpha", 1.0)
+application_parameters.add("solve_primal", True)
+application_parameters.add("solve_dual", True)
+application_parameters.add("estimate_error", True)
+application_parameters.add("uniform_timestep", False)
 application_parameters.parse()
 
-# Save parameters to file
+# Collect parameters
 parameter_info = application_parameters.option_string()
-f = open("adaptivity/paper1.txt", "w")
-f.write("PAPER I \n \n ")
-f.write(parameter_info)
-f.close()
 
 # Constants related to the geometry of the problem
 cavity_length  = 3.0
@@ -53,10 +53,10 @@ structure_top   = 0.5
 inflow_top = 3.0
 inflow_bottom = 2.5
 
-
-# Define boundaries
+# Define boundaries 
 inflow        = "x[1] > %g + DOLFIN_EPS &&  x[1] < %g - DOLFIN_EPS" %(inflow_bottom, inflow_top)
-outflow       = "x[1] > %g + DOLFIN_EPS &&  x[1] < %g - DOLFIN_EPS"  % (inflow_bottom, inflow_top)
+outflow       = "x[0] > %g - DOLFIN_EPS && \
+                 x[1] > %g - DOLFIN_EPS && x[1] < %g - DOLFIN_EPS"  % (cavity_length, inflow_bottom, inflow_top)
 fixed_left    = "x[0] == 0.0  && x[1] >= DOFLIN_EPS" 
 fixed_right   = "x[0] > %g - DOLFIN_EPS  && x[1] >= 0.0" % structure_right
 fixed_bottom  = "x[0] == 0.0" 
@@ -71,21 +71,47 @@ class Structure(SubDomain):
             x[0] < structure_right + DOLFIN_EPS and \
             x[1] < structure_top   + DOLFIN_EPS
 
+# Define problem class
 class Paper1(FSI):
-
     def __init__(self):
 
         ny = application_parameters["ny"]
         nx = ny
         mesh = Rectangle(0.0, 0.0, cavity_length, cavity_height, nx, ny)
 
+        # Report problem parameters
+        mesh_size = mesh.hmin()
+        f = open("adaptivity/paper1.txt", "w")
+        f.write("=========PAPER I ========= \n \n \n")
+        f.write(str("Mesh size:  ") + (str(mesh_size)) + "\n \n")
+        f.write(parameter_info)
+        f.close()
+        f.close()
+
         # Initialize base class
         FSI.__init__(self, mesh)
+
+    #--- Solver options ---
+
+    def solve_primal(self):
+        return application_parameters["solve_primal"]
+
+    def solve_dual(self):
+        return application_parameters["solve_dual"]
+
+    def estimate_error(self):
+        return application_parameters["estimate_error"]
+
+    def uniform_timestep(self):
+        return application_parameters["uniform_timestep"]
 
     #--- Common parameters ---
 
     def end_time(self):
         return application_parameters["T"]
+
+    def TOL(self):
+        return application_parameters["TOL"]
 
     def initial_timestep(self):
         return application_parameters["dt"]
@@ -102,17 +128,15 @@ class Paper1(FSI):
     def dorfler_fraction(self):
         return application_parameters["dorfler_fraction"]
 
-    def TOL(self):
-        return application_parameters["TOL"]
-
     def evaluate_functional(self, u_F, p_F, U_S, P_S, U_M, dt):
 
         # Compute average displacement
-        structure_area = (structure_right - structure_left) * structure_top
+#        structure_area = (structure_right - structure_left) * structure_top
+        structure_area = 1.0
         displacement = (1.0/structure_area)*assemble(U_S[0]*dx + U_S[1]*dx, mesh=U_S.function_space().mesh())
 
         return displacement
-        
+
     def __str__(self):
         return "Paper I" 
 
@@ -125,7 +149,7 @@ class Paper1(FSI):
         return 1.0
 
     def fluid_velocity_dirichlet_values(self):
-        return [(0.0, 0.0), Expression(("x[1]*(x[1] - 2.5)*(2.0/3.0)", "0.0")), (1.0 ,0.0)]
+        return [(0.0, 0.0), Expression(("x[1]*(x[1] - 2.5)*(2.0/3.0)", "0.0")), (1.0, 0.0)]
 
     def fluid_velocity_dirichlet_boundaries(self):
         return [noslip, inflow, flow_top]
@@ -157,7 +181,7 @@ class Paper1(FSI):
         return 0.25*125.0
 
     def structure_dirichlet_values(self):
-        return [(0,0), (0,0)]
+        return [(0.0, 0.0), (0.0, 0.0)]
 
     def structure_dirichlet_boundaries(self):
         return [fixed_left, fixed_right]
@@ -178,11 +202,11 @@ class Paper1(FSI):
 
 # Solve problem
 problem = Paper1()
-problem.parameters["solver_parameters"]["solve_primal"] = True
-problem.parameters["solver_parameters"]["solve_dual"]  =  True
-problem.parameters["solver_parameters"]["estimate_error"] = True
+problem.parameters["solver_parameters"]["solve_primal"] = problem.solve_primal()
+problem.parameters["solver_parameters"]["solve_dual"] = problem.solve_dual() 
+problem.parameters["solver_parameters"]["estimate_error"] = problem.estimate_error()
 problem.parameters["solver_parameters"]["plot_solution"] =  False
-problem.parameters["solver_parameters"]["uniform_timestep"]  = False
+problem.parameters["solver_parameters"]["uniform_timestep"]  = problem.uniform_timestep()
 problem.parameters["solver_parameters"]["tolerance"] = problem.TOL()
 u_F, p_F, U_S, P_S, U_M = problem.solve()
 
