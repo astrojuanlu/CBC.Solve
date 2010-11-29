4571d58e3c469dc62ef0db91c4623b535989dba3
selim <selim@selim-laptop>
2010-11-29 19:03:28 +0100

    Added more parameters in BB script

--- sandbox/fsi/run_bb.py
+++ sandbox/fsi/run_bb.py
@@ -5,18 +5,25 @@ os.environ['PYTHONPATH'] = "..:../..:" + os.environ['PYTHONPATH']
 days = 20
 
 # Define problem names
-problem_names = ["channel_with_flap", "driven_cavity_free_bottom", "driven_cavity_fixed_bottom" , "leaky_cavity_free_bottom", "leaky_cavity_fixed_bottom"]
+problem_names = ["paper1", "channel_with_flap", "driven_cavity_free_bottom", "driven_cavity_fixed_bottom" , "leaky_cavity_free_bottom", "leaky_cavity_fixed_bottom"]
 
 #-----------------------------------------------
-# problem_names[0] = channel_with_flap
-# problem_names[1] = driven_cavity_free_bottom
-# problem_names[2] = driven_cavity_fixed_bottom
-# problem_names[3] = leaky_cavity_free_bottom
-# problem_names[4] = leaky_cavity_fixed_bottom
+# problem_names[0] = paper1
+# problem_names[1] = channel_with_flap
+# problem_names[2] = driven_cavity_free_bottom
+# problem_names[3] = driven_cavity_fixed_bottom
+# problem_names[4] = leaky_cavity_free_bottom
+# problem_names[5] = leaky_cavity_fixed_bottom
 #-----------------------------------------------
 
 # Define problem
-problem = problem_names[2]
+problem = problem_names[0]
+
+# Define solver options
+solve_primal     = True
+solve_dual       = True
+estimate_error   = True
+uniform_timestep = True
 
 # Define TOL parameters
 TOL = 0.1
@@ -31,11 +38,11 @@ ny  = 20
 
 # Define time parameters
 dt  = 0.02
-T   = 5.0
+T   = 2.0
 
 # Define and submit job (and clear old data)
 clean = ("./clean.sh")
-jobs =("python"+" "+ str(problem)+".py"+" "+"--w_h"+" "+ str(w_h)+" "+"--w_k"+" "+ str(w_k) +" "+"--w_c"+" "+ str(w_c) +" "+"--T"+" "+ str(T)  +" "+"--dt"+" "+ str(dt)  +" "+"--dorfler_fraction"+" "+ str(d_f) +" "+"--mesh_alpha"+" "+ str(m_a)+" "+"--adaptive_tolerance"+" "+ str(TOL))
+jobs =("python"+" "+ str(problem)+".py"+" "+"--w_h"+" "+ str(w_h)+" "+"--w_k"+" "+ str(w_k) +" "+"--w_c"+" "+ str(w_c) +" "+"--T"+" "+ str(T)  +" "+"--dt"+" "+ str(dt)  +" "+"--dorfler_fraction"+" "+ str(d_f) +" "+"--mesh_alpha"+" "+ str(m_a)+" "+"--TOL"+" "+ str(TOL) +"--solve_primal"+" "+ str(solve_primal)  +"--solve_dual"+" "+ str(solve_dual) +"--estimate_error"+" "+ str(estimate_error)  +"--uniform_timestep"+" "+ str(uniform_timestep))
 submit(clean, nodes=1, ppn=8,  keep_environment=True, walltime=24*days, dryrun=False)
 sleep(1)
 submit(jobs, nodes=1, ppn=8,  keep_environment=True, walltime=24*days, dryrun=False)
