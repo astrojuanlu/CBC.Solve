32c8c2a832daa40c4b17ffd590e58d720a142cd6
selim <selim@selim-laptop>
2010-12-15 12:56:47 +0100

    Changed clean.sh so we do not remove the reference values

--- sandbox/fsi/clean.sh
+++ sandbox/fsi/clean.sh
@@ -1,5 +1,11 @@
 rm -r bin
 rm -r pvd
 rm -r adaptivity/*.xml
-rm -r adaptivity/*.txt
-rm adaptivity/adaptivity.log
+rm -r adaptivity/no_iterations.txt
+rm -r adaptivity/fsi_tolerance.txt
+rm -r adaptivity/goal_functional.txt
+rm -r adaptivity/timesteps.txt
+rm -r adaptivity/M_ave.txt
+rm -r adaptivity/num_dofs.txt
+rm -r adaptivity/error_estimates.txt
+rm -r adaptivity/adaptivity.log
