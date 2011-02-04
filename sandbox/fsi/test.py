44b7746869ce175bf54e596cb72f8485aa9eb962
Anders Logg <logg@simula.no>
2011-02-04 14:45:58 +0100

    Updates

--- sandbox/fsi/test.py
+++ sandbox/fsi/test.py
@@ -1,16 +1,6 @@
 from dolfin import *
 
-application_parameters = Parameters("application_parameters")
+mesh = UnitSquare(2, 2)
+cell = Cell(mesh, 0)
 
-#application_parameters.add("end_time", 0.25)
-#application_parameters.add("dt", 0.01)
-#application_parameters.add("mesh_scale", 1)
-#application_parameters.add("TOL", 1e-12)
-#application_parameters.add("w_h", 0.45)
-#application_parameters.add("w_k", 0.45)
-#application_parameters.add("w_c", 0.1)
-
-file = File("application_parameters.xml")
-file >> application_parameters
-
-info(application_parameters, True)
+print cell.num_entities(0), len(cell.entities(0))
