f0141196ab10bb4f26ef7e39dbf472d0a7347b4b
Anders Logg <logg@simula.no>
2011-02-06 23:36:41 +0100

    Update adaptive_poisson.py (by Marie)

--- sandbox/fsi/adaptive_poisson.py
+++ sandbox/fsi/adaptive_poisson.py
@@ -6,7 +6,7 @@ __license__  = "GNU GPL Version 3 or any later version"
 
 from dolfin import *
 from numpy import max
-from pylab import *
+import pylab
 
 f = Expression("1")
 
@@ -37,53 +37,36 @@ def evaluate_jumps(u):
     assemble(eta, tensor=j.vector())
 
     print "Maximum jump:", max(j.vector().array())
+    print "Maximum cell size:", mesh.hmax()
 
     return j
 
-def refine_mesh(mesh, j):
-    "Refine mesh based on jumps"
-
-    #return refine(mesh)
-
-    fraction = 0.5
-    indicators = j.vector().array()
-    indices = list(argsort(indicators))
-    indices.reverse()
-    markers = CellFunction("bool", mesh)
-    markers.set_all(False)
-    for index in indices[:int(round(fraction*len(indices)))]:
-        markers[int(index)] = True
-    markers.set_all(True)
-    return refine(mesh, markers)
-
 mesh = UnitSquare(2, 2)
-num_refinements = 7
+num_refinements = 12
 file = File("jumps.pvd")
 jumps = []
+hmaxs = []
 
 for level in range(num_refinements + 1):
-
-    print
-    print "Level", level
-    print
-
     u = solve(mesh)
     j = evaluate_jumps(u)
 
-    #plot(u, title="Solution")
-    #plot(j, title="Jumps")
-
-    jumps.append(max(j.vector().array()))
+    jumps.append(max(abs(j.vector().array())))
+    hmaxs.append(mesh.hmax())
     file << j
 
-    mesh = refine_mesh(mesh, j)
+    markers = CellFunction("bool", mesh)
+    markers.set_all(True)
+    mesh = refine(mesh, markers)
 
 print
 print "Maximum jumps:", str(jumps)
 
-semilogy(range(num_refinements + 1), jumps, '-o')
-grid(True)
-xlabel("Refinement level")
-ylabel("Maximum squared jump indicator")
-title("Simple Poisson model problem")
-show()
+import pylab
+pylab.loglog(hmaxs, jumps, '-o')
+pylab.grid(True)
+pylab.xlabel("h max")
+pylab.ylabel("Maximum squared jump indicator")
+pylab.title("Simple Poisson model problem")
+pylab.savefig("jumps.png")
+pylab.show()
