3edaaae8bd1c42bd08bc6ecce16fada04f8bdc09
selim <selim@selim-laptop>
2011-02-08 09:48:23 +0100

    Clean up and removed some bugs. Works fine, test the drivencavity problem.

--- sandbox/splitting_error/storage.py
+++ sandbox/splitting_error/storage.py
@@ -8,23 +8,23 @@ __author__ = "Kristoffer Selim and Anders Logg"
 __copyright__ = "Copyright (C) 2010 Simula Research Laboratory and %s" % __author__
 __license__  = "GNU GPL Version 3 or any later version"
 
-# Last changed: 2011-01-30
+# Last changed: 2011-02-08
 
 from numpy import append
 from dolfin import *
 
 def create_primal_series():
     "Create time series for primal velocity"
-    u = TimeSeries("bin/u")
-    p = TimeSeries("bin/p")
-    return u, p
+    u_primal_data = TimeSeries("bin/u_primal_data")
+    p_primal_data = TimeSeries("bin/p_primal_data")
+    return (u_primal_data, p_primal_data)
 
 def create_dual_series():
     "Create time series for dual solution"
     dual_sol = TimeSeries("bin/dual_sol")
     return dual_sol
 
-def read_primal_data(U, t, Omega, series):
+def read_primal_data(U, t, series):
     "Read primal variables at given time"
 
     info("Reading primal data at t = %g" % t)
@@ -32,14 +32,18 @@ def read_primal_data(U, t, Omega, series):
     # Get primal variables
     (u, p) = U
 
-    # Create vectors for 
-    u = Vector()
-    p = Vector()
+    # Create vectors 
+    u_values = Vector()
+    p_values = Vector()
 
     # Retrieve primal data
-    series[0].retrieve(u, t)
-    series[1].retrieve(p, t)
+    series[0].retrieve(u_values, t)
+    series[1].retrieve(p_values, t)
 
+    # Copy to vector
+    u.vector()[:] = u_values
+    p.vector()[:] = p_values
+    
 def read_dual_data(z, y, t, series):
     "Read dual solution at given time"
     info("Reading dual data at t = %g" % t)
@@ -52,17 +56,19 @@ def read_timestep_range(T, series):
     # Get nodal points for primal time series
     t = series[0].vector_times()
 
-    # Check that time series is not empty and covers the interval
-    if not (len(t) > 1 and t[0] == 0.0 and t[-1] == T):
-        print "Nodal points for primal time series:", t
-        raise RuntimeError, "Missing primal data"
+    # Check that time series is not empty and that it covers the interval
+    if len(t) == 0:
+        error("Missing primal data (empty).")
+    elif t[0] > DOLFIN_EPS:
+        error("Illegal initial value %.16e for primal data, expecting 0.0." % t[0])
+    elif t[-1] < T - DOLFIN_EPS:
+        error("Illegal final time %.16e for primal data, expecting (at least) %.16e" % (t[-1], T))
 
     return t
 
 def write_primal_data(U, t, series):
     "Write primal data at given time"
     [series[i].store(U[i].vector(), t) for i in range(2)]
-
 
 def write_dual_data(dual_sol, t, series):
     "Write dual solution at given time"
