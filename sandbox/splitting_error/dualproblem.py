6583c6cd2a93863fdc8abe4eccdfc9bdffb1b8d3
selim <selim@selim-laptop>
2011-02-08 11:13:08 +0100

    Added functions in operators.py

--- sandbox/splitting_error/dualproblem.py
+++ sandbox/splitting_error/dualproblem.py
@@ -7,6 +7,7 @@ __license__  = "GNU GPL Version 3 or any later version"
 # Last changed: 2011-02-08
 
 from dolfin import *
+from operators import *
 
 def create_dual_forms(problem, Omega, k,
                       v, q, z, y, z0, 
@@ -25,28 +26,17 @@ def create_dual_forms(problem, Omega, k,
 
     # Define facet normal
     n = FacetNormal(Omega)
-
-    # Define identity matrix in 2D
-    I = Identity(2)
-    
-    # Define the symetric gradient
-    def epsilon(v):
-        return 0.5*(grad(v) + grad(v).T)
-
-    # Define the fluid stress tensor
-    def sigma(v, q):
-        return  2*mu*epsilon(v) - q*I
     
     # FIXME: Talk to Anders/Mats about this cG(1) formulation 
     # Defined mid point value for the fluid 
-    u_mid = 0.5*(uh0 + uh1)
+    U = 0.5*(uh0 + uh1)
 
     # Define the dual momemtum form
     dual_mom = -(1/k)*rho*inner((z0 - z), v)*dx \
-               + rho*inner(z, dot(grad(u_mid), v))*dx \
-               + rho*inner(z, dot(grad(v), u_mid))*dx \
-               + inner(epsilon(z), sigma(v,q))*dx \
-               - inner(z, dot(sigma(v,q),n ))*ds
+               + rho*inner(z, dot(grad(U), v))*dx \
+               + rho*inner(z, dot(grad(v), U))*dx \
+               + inner(epsilon(z), sigma(v,q,mu))*dx \
+               - inner(z, dot(sigma(v,q,mu), n))*ds
      
     # Define the dual continuity form
     dual_cont = inner(y, div(v))*dx
