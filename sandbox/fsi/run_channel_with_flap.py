1afaf4d18e119c828feea33b5e514aeea1766575
Anders Logg <logg@simula.no>
2011-02-11 13:03:19 +0100

    Improve run script for channel with flap

--- sandbox/fsi/run_channel_with_flap.py
+++ sandbox/fsi/run_channel_with_flap.py
@@ -8,11 +8,22 @@ p["estimate_error"] = True
 p["uniform_timestep"] = True
 p["tolerance"] = 1e-6
 p["initial_timestep"] = 0.05
-#p["dorfler_marking"] = False
-#p["marking_fraction"] = 0.5
-#p["refinement_algorithm"] = "recursive_bisection"
+
+# test case 1
 p["refinement_algorithm"] = "regular_cut"
-p["description"] = "adaptive, k = 0.025 (regular)"
+p["description"] = "adaptive, k = 0.05 (regular)"
+p["output_directory"] = "results-channel_with_flap_1"
+
+# test case 2
+#p["refinement_algorithm"] = "recursive_bisection"
+#p["description"] = "adaptive, k = 0.05 (bisection)"
+#p["output_directory"] = "results-channel_with_flap_2"
+
+# test case 3
+#p["dorfler_marking"] = False
+#p["refinement_algorithm"] = "regular_cut"
+#p["description"] = "adaptive, k = 0.05 (regular, fixed fraction)"
+#p["output_directory"] = "results-channel_with_flap_3"
 
 # Run problem
 run_local("channel_with_flap", p)
