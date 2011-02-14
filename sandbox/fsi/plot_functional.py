0e543ed7e781c2029764ae0cb0867205cd5c6d4d
Anders Logg <logg@simula.no>
2011-02-14 12:10:13 +0100

    Improvements of plot script for functionals

--- sandbox/fsi/plot_functional.py
+++ sandbox/fsi/plot_functional.py
@@ -5,10 +5,18 @@ __copyright__ = "Copyright (C) 2010 Simula Research Laboratory and %s" % __autho
 __license__  = "GNU GPL Version 3 or any later version"
 
 import os, glob
+from numpy import argsort, array
 from pylab import *
 
+# Set reference value
+#reference_value = None
+reference_value = 0.003656
+
 def read(filename):
-    return zip(*[row.split(" ") for row in open(filename).read().split("\n") if " " in row])
+    return zip(*[[x for x in row.split(" ") if len(x) >0] for row in open(filename).read().split("\n") if " " in row and len(row) > 0])
+
+def read_float(filename):
+    return array([[float(x) for x in column] for column in read(filename)])
 
 # Check all results directories
 functionals = []
@@ -22,13 +30,13 @@ for directory in glob.glob("results-*"):
     if not os.path.isfile(filename): continue
 
     # Get functional values
-    f = read(filename)[2]
+    f = read_float(filename)[2]
     print "Found %d values" % len(f)
     functionals.append(f)
 
     # Get number of dofs
     filename = "%s/num_dofs.txt" % directory
-    d = read(filename)[2]
+    d = read_float(filename)[2]
     dofs.append(d)
 
     # Get legend
@@ -37,22 +45,41 @@ for directory in glob.glob("results-*"):
     l = row.split('value="')[1].split('"')[0]
     legends.append(l)
 
-# Group values
+# Sort values by legend
+indices = argsort(legends)
+legends = [legends[i] for i in indices]
+
+ # Group values
 plotvals = []
-for (x, y) in zip(dofs, functionals):
+for i in indices:
+    x = dofs[i]
+    if reference_value is None:
+        y = functionals[i]
+    else:
+        y = [abs(value - reference_value) for value in functionals[i]]
     if not len(x) == len(y):
         raise RuntimeError, ("Data size mismatch: %d %d" % (len(x), len(y)))
     plotvals.append(x)
     plotvals.append(y)
 
+# Set fontsize for legend
+rcParams.update({'legend.fontsize': 11})
+
 # Plot functional values
-figure(1)
-subplot(2, 1, 1)
-semilogx(*plotvals, marker='o')
-xlabel("Number of dofs (in space)")
-ylabel("Functional value")
-title("Convergence of functional value")
-legend(legends, bbox_to_anchor=(-0.1, -0.3), loc=2, borderaxespad=0)
-grid(True)
+max_plots = 9
+for i in range((len(legends) - 1) / max_plots + 1):
+    xy = plotvals[2*i*max_plots:2*(i + 1)*max_plots]
+    figure(i)
+    subplot(2, 1, 1)
+    if reference_value is None:
+        ylabel("Functional value")
+        semilogx(*xy, marker='o')
+    else:
+        ylabel("Error in functional value (absolute value)")
+        loglog(*xy, marker='o')
+    xlabel("Number of dofs (in space)")
+    title("Convergence of functional value")
+    legend(legends[i*max_plots:(i + 1)*max_plots], bbox_to_anchor=(-0.1, -0.3), loc=2, borderaxespad=0)
+    grid(True)
 
 show()
