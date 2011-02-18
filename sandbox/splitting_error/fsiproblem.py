b9009bdb0efcfc47f46b9610c8eba8cc14b8993c
selim@simula.no <>
2011-02-18 12:21:07 +0100

    Cleaned up fsiproblem. It only returns the fluid mesh as Omega

--- sandbox/splitting_error/fsiproblem.py
+++ sandbox/splitting_error/fsiproblem.py
@@ -2,7 +2,7 @@ __author__ = "Kristoffer Selim andAnders Logg"
 __copyright__ = "Copyright (C) 2010 Simula Research Laboratory and %s" % __author__
 __license__  = "GNU GPL Version 3 or any later version"
 
-# Last changed: 2010-09-13
+# Last changed: 2011-02-18
 
 from dolfin import *
 from numpy import array, append
@@ -43,106 +43,9 @@ class FSI(CBCProblem):
         # Set global mesh
         self.Omega = Omega
 
-#         # Create cell markers (0 = fluid, 1 = structure)
-#         D = Omega.topology().dim()
-#         cell_domains = MeshFunction("uint", self.Omega, D)
-#         cell_domains.set_all(0)
-#        structure = self.structure()
-#        structure.mark(cell_domains, 1)
 
-        # Extract submeshes for fluid and structure
-#        Omega_F = SubMesh(self.Omega, cell_domains, 0)
-#        Omega_F = Omega
-#        Omega_S = SubMesh(self.Omega, cell_domains, 1)
-
-#        info("Computing mappings between submeshes")
-
-#         # Extract matching indices for fluid and structure
-#         structure_to_fluid = compute_vertex_map(Omega_S, Omega_F)
-
-#         # Extract matching indices for fluid and structure
-#         fluid_indices = array([i for i in structure_to_fluid.itervalues()])
-#         structure_indices = array([i for i in structure_to_fluid.iterkeys()])
-
-#         # Extract matching dofs for fluid and structure (for vector P1 elements)
-#         fdofs = append(fluid_indices, fluid_indices + Omega_F.num_vertices())
-#         sdofs = append(structure_indices, structure_indices + Omega_S.num_vertices())
-
-#         info("Computing FSI boundary and orientation markers")
-
-#         # Initialize FSI boundary and orientation markers
-#         Omega.init(D - 1, D)
-#         fsi_boundary = MeshFunction("uint", Omega, D - 1)
-#         fsi_boundary.set_all(0)
-#         fsi_orientation = Omega.data().create_mesh_function("facet orientation", D - 1)
-#         fsi_orientation.set_all(0)
-
-#         # Compute FSI boundary and orientation markers
-#         for facet in facets(Omega):
-
-#             # Skip facets on the boundary
-#             if facet.num_entities(D) == 1:
-#                 continue
-
-#             # Create the two cells
-#             c0, c1 = facet.entities(D)
-#             cell0 = Cell(Omega, c0)
-#             cell1 = Cell(Omega, c1)
-
-#             # Get the two midpoints
-#             p0 = cell0.midpoint()
-#             p1 = cell1.midpoint()
-
-#             # Check if the points are inside
-#             p0_inside = structure.inside(p0, False)
-#             p1_inside = structure.inside(p1, False)
-
-#             # Just set c0, will be set only for FSI facets below
-#             fsi_orientation[facet.index()] = c0
-
-#             # Look for points where exactly one is inside the structure
-#             if p0_inside and not p1_inside:
-#                 fsi_boundary[facet.index()] = 2
-#                 fsi_orientation[facet.index()] = c1
-#             elif p1_inside and not p0_inside:
-#                 fsi_boundary[facet.index()] = 2
-#                 fsi_orientation[facet.index()] = c0
-#             elif p0_inside and p1_inside:
-#                 fsi_boundary[facet.index()] = 1
-#             else:
-#                 fsi_boundary[facet.index()] = 0
-
-        # Store data
-#        self.Omega_F = Omega_F
-#        self.Omega_S = Omega_S
-#         self.cell_domains = cell_domains
-#         self.fdofs = fdofs
-#         self.sdofs = sdofs
-#         self.fsi_boundary = fsi_boundary
-#         self.fsi_orientation = fsi_orientation
-
-#     def mesh(self):
-#         "Return mesh for full domain"
-#         return self.Omega
-
+    # Return the fluid mesh as Omege
     def fluid_mesh(self):
         "Return mesh for fluid domain"
         return self.Omega
 
-#     def structure_mesh(self):
-#         "Return mesh for structure domain"
-#         return self.Omega_S
-
-#     def add_f2s(self, xs, xf):
-#         "Compute xs += xf for corresponding indices"
-#         xs_array = xs.array()
-#         xf_array = xf.array()
-#         xs_array[self.sdofs] += xf_array[self.fdofs]
-#         xs[:] = xs_array
-
-#     def add_s2f(self, xf, xs):
-#         "Compute xf += xs for corresponding indices"
-#         xf_array = xf.array()
-#         xs_array = xs.array()
-#         xf_array[self.fdofs] += xs_array[self.sdofs]
-#         xf[:] = xf_array
