8a231b7885298d33f6e63bd0dab57d6990b40476
Anders Logg <logg@simula.no>
2011-02-18 08:26:55 +0100

    Increase maximum number of refinements to 100 in cluster script

--- sandbox/fsi/run_channel_with_flap_bb.py
+++ sandbox/fsi/run_channel_with_flap_bb.py
@@ -1,7 +1,7 @@
 from fsirun import *
 
 # Cases to investigate
-q_range = [1, 2]
+q_range = [1]
 k_range = [0.01, 0.005]
 r_range = ["uniform", "recursive_bisection", "regular_cut"]
 d_range = [None, True, False]
@@ -15,8 +15,8 @@ for q in q_range:
             for d in d_range:
                 for f in f_range:
 
-                    if case > 0:
-                        continue
+                    #if case > 0:
+                    #    continue
 
                     # Handle d and f cases not relevant for uniform refinement
                     if r == "uniform" and not (d is None and f is None): continue
@@ -30,6 +30,7 @@ for q in q_range:
                     p["uniform_timestep"] = True
                     p["tolerance"] = 1e-8
                     p["fixedpoint_tolerance"] = 1e-12
+                    p["max_num_refinements"] = 100
 
                     # Set parameters for current case
                     p["structure_element_degree"] = q
