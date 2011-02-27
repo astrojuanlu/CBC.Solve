880aa93ac543e0d1e9856174d69b7be70545cdb8
Anders Logg <logg@simula.no>
2011-02-27 18:28:56 +0100

    Adjust parameters in run script for channel_with_flap

--- sandbox/fsi/run_channel_with_flap.py
+++ sandbox/fsi/run_channel_with_flap.py
@@ -2,40 +2,8 @@ from fsirun import *
 
 # Set common parameters
 p = default_parameters()
-p["solve_primal"] = True
-p["solve_dual"] = True
-p["estimate_error"] = True
-p["tolerance"] = 1e-5
+p["tolerance"] = 1e-8
 p["initial_timestep"] = 0.05
 
-# Choose test case
-case = 0
-
-# Test cases
-if case is 0:
-    p["description"] = "adaptive time steps"
-elif case is 1:
-    p["uniform_timestep"] = True
-    p["description"] = "adaptive, k = 0.01 (regular)"
-    p["refinement_algorithm"] = "regular_cut"
-elif case is 2:
-    p["uniform_timestep"] = True
-    p["description"] = "adaptive, k = 0.01 (regular, fixed fraction)"
-    p["refinement_algorithm"] = "regular_cut"
-    p["dorfler_marking"] = False
-elif case is 3:
-    p["uniform_timestep"] = True
-    p["description"] = "adaptive, k = 0.01 (bisection)"
-    p["refinement_algorithm"] = "recursive_bisection"
-elif case is 4:
-    p["uniform_timestep"] = True
-    p["description"] = "adaptive, k = 0.01 (bisection, fixed fraction)"
-    p["refinement_algorithm"] = "recursive_bisection"
-    p["dorfler_marking"] = False
-elif case is 5:
-    p["uniform_timestep"] = True
-    p["description"] = "uniform, k = 0.01"
-    p["uniform_mesh"] = True
-
 # Run problem
-run_local("channel_with_flap", parameters=p, case=case)
+run_local("channel_with_flap", parameters=p, case=0)
