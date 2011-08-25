4369d7c127a3cc90f228a633e20da2740bc1e449
selim@simula.no <>
2011-08-25 13:45:00 +0200

    Added refinement strategy

--- sandbox/aipcs/run_lid_driven_cavity.py
+++ sandbox/aipcs/run_lid_driven_cavity.py
@@ -5,12 +5,14 @@ case = 1
 # Set common parameters
 p = default_parameters()
 p["solve_primal"] = True
-p["solve_dual"] = False
-p["estimate_error"] = False
-p["uniform_timestep"] = True
+p["solve_dual"] = True
+p["estimate_error"] = True
+p["uniform_timestep"] = False
 p["tolerance"] = 1e-7
-p["initial_timestep"] = 0.01
+p["initial_timestep"] = 0.05
 p["goal_functional"] = case
+p["refinement_algorithm"] = "recursive_bisection"
+#p["refinement_algorithm"] = "regular_cut"
 
 # Run problem
 run_local("lid_driven_cavity", parameters=p, case=case)
