888dfb84bff703e53e71199578e2435af2615f75
selim@simula.no <>
2011-08-25 12:59:04 +0200

    More work in driven cavity

--- sandbox/aipcs/lid_driven_cavity.py
+++ sandbox/aipcs/lid_driven_cavity.py
@@ -9,11 +9,11 @@ from fsiproblem import *
 # Read parameters
 application_parameters = read_parameters()
 
-# Define inflow boundary
-inflow = "x[0] == 0.0 && x[1] < 1.0 - DOLFIN_EPS"
+# Define top boundary
+top = "near(x[1], 1.0)"
 
 # Define noslip boundary
-noslip = "on_boundary && !(%s)" % inflow
+noslip = "on_boundary && !(%s)" % top
 
 class LidDrivenCavity(FSI):
 
@@ -25,16 +25,7 @@ class LidDrivenCavity(FSI):
         ny = n
 
         # Create mesh
-        mesh = Rectangle(0.0, 0.0, 1.0, 1.0, nx, ny)
-        # cell_domains = CellFunction("uint", mesh)
-        # cell_domains.set_all(0)
-        # structure = Structure()
-        # structure.mark(cell_domains, 1)
-        # mesh = SubMesh(mesh, cell_domains, 0)
-
-        # Create subdomains for goal functionals
-        # self.outflow_domain = compile_subdomains(outflow)
-        # self.top_domain = compile_subdomains(top)
+        mesh = UnitSquare(nx, ny)
 
         # Create Riesz representer for goal functional
         self.psi = Expression("c*exp(-((x[0] - x0)*(x[0] - x0) + (x[1] - x1)*(x[1] - x1)) / (2.0*r*r))", c = 1.0, r = 0.15, x0 = 0.5, x1 = 0.3)
@@ -52,21 +43,17 @@ class LidDrivenCavity(FSI):
     #--- Common ---
 
     def end_time(self):
-        return 2.5
+        return 1.0
 
     def evaluate_functional(self, u, p):
 
-        # Goal functional 1: integration against Gaussian
-        if application_parameters["goal_functional"] == 1:
-            info("Goal functional is integration against Gaussian")
-
-            c /= assemble(self.psi*dx, mesh=self.Omega)
-            return u[0]*self.psi*dx, None, None, None
+        self.psi.c /= assemble(self.psi*dx, mesh=self.Omega)
+        return u[0]*self.psi*dx, None, None, None
 
     def __str__(self):
-        return "Lid driven cavity"
+        return "Lid-driven cavity problem"
 
-    #--- Fluid problem ---
+    #--- Fluid specific parameters ---
 
     def fluid_density(self):
         return 1.0
@@ -74,20 +61,20 @@ class LidDrivenCavity(FSI):
     def fluid_viscosity(self):
         return 1.0
 
-    def fluid_velocity_dirichlet_values(self):
-        return [(0.0, 0.0)]
-
     def fluid_velocity_dirichlet_boundaries(self):
-        return [noslip]
+        return [noslip, top]
 
-    def fluid_pressure_dirichlet_values(self):
-        return [0.0]
+    def fluid_velocity_dirichlet_values(self):
+        return [(0.0, 0.0), Expression(("4.0*x[0]*(1.0 - x[0])", "0.0"))]
 
     def fluid_pressure_dirichlet_boundaries(self):
-        return [inflow]
+        return []
+
+    def fluid_pressure_dirichlet_values(self):
+        return []
 
     def fluid_velocity_initial_condition(self):
-        return "4.0*x[1]*(1-x[1])"
+        return [0.0, 0.0]
 
     def fluid_pressure_initial_condition(self):
         return 0.0
