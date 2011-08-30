ed2a426273716ff31d7d32fac4c43eef920545d7
selim@simula.no <>
2011-08-30 20:04:50 +0200

    Final changes in driven cavity

--- sandbox/aipcs/lid_driven_cavity.py
+++ sandbox/aipcs/lid_driven_cavity.py
@@ -2,7 +2,7 @@ __author__ = "Kristoffer Selim and Anders Logg"
 __copyright__ = "Copyright (C) 2011 Simula Research Laboratory and %s" % __author__
 __license__  = "GNU GPL Version 3 or any later version"
 
-# Last changed: 2011-08-28
+# Last changed: 2011-08-30
 
 
 from fsiproblem import *
@@ -10,33 +10,22 @@ from fsiproblem import *
 # Read parameters
 application_parameters = read_parameters()
 
-# Define the top boundary
-top = "near(x[1], 2.0)"
+# Define inflow at the top
+top = "near(x[1], 1.0)"
 
 # Define noslip boundary
 noslip = "on_boundary && !(%s)" % top
-
-# Define the inflow boundary for regulized profile
-top_left   = "x[0] == 0.0 && x[0] < 0.25 - DOLFIN_EPS &&\
-              near(x[1], 2.0)"
-top_middle = "x[0] > 0.25 + DOLFIN_EPS  && x[0] < 1.75 - DOLFIN_EPS &&\
-              near(x[1], 2.0)"
-top_right  = "x[0] > 1.75 && x[0] <= 2 &&\
-              near(x[1], 2.0)"
 
 class LidDrivenCavity(FSI):
 
     def __init__(self):
 
-        # Number of inital elements
-        n = 40
-        nx, ny = n, n
-
-        # Create mesh
-        mesh = Rectangle(0.0, 0.0, 2.0, 2.0, nx, ny)
+        # Create inital mesh
+        n = 2
+        mesh = UnitSquare(n, n)
 
         # Create Riesz representer for goal functional
-        self.psi = Expression("c*exp(-((x[0] - x0)*(x[0] - x0) + (x[1] - x1)*(x[1] - x1)) / (2.0*r*r))", c = 1.0, r = 0.15, x0 = 1.5, x1 = 1.25)
+        self.psi = Expression("c*exp(-((x[0] - x0)*(x[0] - x0) + (x[1] - x1)*(x[1] - x1)) / (2.0*r*r))", c = 1.0, r = 0.15, x0 = 0.75, x1 = 0.75)
 
         # Initialize base class
         FSI.__init__(self, mesh)
@@ -44,7 +33,7 @@ class LidDrivenCavity(FSI):
     #--- Common ---
 
     def end_time(self):
-        return 2.5
+        return 1.0
 
     def evaluate_functional(self, u, p):
         self.psi.c /= assemble(self.psi*dx, mesh=self.Omega)
@@ -62,10 +51,13 @@ class LidDrivenCavity(FSI):
         return 1.0
 
     def fluid_velocity_dirichlet_boundaries(self):
-        return [noslip, top_left, top_middle, top_right]
+        return [noslip, top]
 
     def fluid_velocity_dirichlet_values(self):
-        return [(0.0, 0.0), Expression(("4*pow(x[1], p)", "0.0"), p=1), Expression(("1.0", "0.0")), Expression(("2*pow(2-x[1], p)", "0.0"), p=1)]
+        return [(0.0, 0.0), Expression(("x[0]*(1.0 - x[0])", "0.0"))]
+
+        # Mats and Fredik's profile
+        # return [(0.0, 0.0), Expression(("1.0 - pow(1.0 - x[0], p)", "0.0"), p=18)]
 
     def fluid_pressure_dirichlet_boundaries(self):
         return [top]
