afe48eb6f9a634d372b112c92ef1d504fbc09ef8
Kent-Andre Mardal <kent-and@simula.no>
2011-09-29 08:25:03 +0200

    update

--- demo/flow/aneurysm.py
+++ demo/flow/aneurysm.py
@@ -8,21 +8,28 @@ from cbc.flow import *
 
 class Inflow(Expression):
   
-    def __init__(self):
-	v  = array([    0.,    27.,    42.,    58.,    69.,    88.,   110.,   130.,                                                                    
+    def __init__(self, V, problem):
+
+        self.problem = problem
+        self.V = problem.V
+        self.mesh = problem.V.mesh() 
+        self.t_period = 1 
+
+	t  = array([    0.,    27.,    42.,    58.,    69.,    88.,   110.,   130.,                                                                    
          136.,   168.,   201.,   254.,   274.,   290.,   312.,   325.,                                                                                      
          347.,   365.,   402.,   425.,   440.,   491.,   546.,   618.,                                                                                      
          703.,   758.,   828.,   897.,  1002.])/(75/60.0)/1000
  
 	scale = 750	
 	#Create interpolated mean velocity in time
-	t = array([ 390.        ,  398.76132931,  512.65861027,  642.32628399,                                                        
+	v = array([ 390.        ,  398.76132931,  512.65861027,  642.32628399,                                                        
 	      710.66465257,  770.24169184,  779.00302115,  817.55287009,                                                                                          
 	      877.12990937,  941.96374622,  970.        ,  961.2386707 ,                                                                                          
 	      910.42296073,  870.12084592,  843.83685801,  794.7734139 ,                                                                                          
 	      694.89425982,  714.16918429,  682.62839879,  644.07854985,                                                                                          
 	      647.58308157,  589.75830816,  559.96978852,  516.16314199,                                                                                          
 	      486.37462236,  474.10876133,  456.58610272,  432.05438066,  390.        ])/574.211239628*scale
+
 	
         self.inflow = splrep(t, v)
 
@@ -30,13 +37,12 @@ class Inflow(Expression):
     def eval_cell(self, values, x, ufc_cell):
         cell = Cell(self.mesh, ufc_cell.index)
         n = cell.normal(ufc_cell.local_facet)
+
 	t = self.problem.t
-	val = splev(t - int(t/self.t_period)*self.t_period,
-	self.bc_func)
+	val = splev(t - int(t/self.t_period)*self.t_period, self.inflow)
 	values[0] = -n.x()*val
 	values[1] = -n.y()*val
 	values[2] = -n.z()*val
-
 
 
     def value_shape(self):
@@ -48,10 +54,11 @@ class Aneurysm(NavierStokes):
         return Mesh("logg.xml.gz")
 
     def viscosity(self):
-        return 0.00345 
+        return 3.5
+#        return 0.00345 
 
     def velocity_dirichlet_values(self):
-        self.g = BoundaryValue()
+        self.g = Inflow(self)
         return [self.g]
 
     def velocity_dirichlet_boundaries(self):
