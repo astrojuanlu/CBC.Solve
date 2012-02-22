6cf176fbb51afd690ff7af4e8a435911d4012b2a
selim@simula.no <>
2012-02-22 17:11:21 +0100

    Structure subroblem defined

--- demo/swing/updown.py
+++ demo/swing/updown.py
@@ -11,7 +11,11 @@ application_parameters = read_parameters()
 fluid_left = "x[0] < DOLFIN_EPS && x[1] > 0.5 - DOLFIN_EPS"
 fluid_top  = "x[1] > 1.0 - DOLFIN_EPS"
 fluid_right = "x[0] > 1.0 - DOLFIN_EPS && x[1] > 0.5 - DOLFIN_EPS"
+
 fluid_bottom = "fabs(x[1] - 0.5) < DOLFIN_EPS"
+
+solid_left = "x[0] < DOLFIN_EPS && x[1] < 0.5 + DOLFIN_EPS"
+solid_right = "x[0] > 1.0 - DOLFIN_EPS && x[1] < 0.5 + DOLFIN_EPS"
 solid_bottom = "x[1] < DOLFIN_EPS"
 
 # Define structure subdomain
@@ -35,7 +39,7 @@ class UpDown(FSI):
     #--- Common ---
 
     def end_time(self):
-        return 0.10
+        return 10.0
 
     def evaluate_functional(self, u_F, p_F, U_S, P_S, U_M, dx_F, dx_S, dx_M):
         # FIXME: Add correct form
@@ -58,7 +62,7 @@ class UpDown(FSI):
     def fluid_velocity_dirichlet_boundaries(self):        return [fluid_bottom]
 
     def fluid_pressure_dirichlet_values(self):
-        return 1.0, 0.0
+        return 0.0, 0.0
 
     def fluid_pressure_dirichlet_boundaries(self):
         return fluid_top, fluid_right
@@ -67,7 +71,7 @@ class UpDown(FSI):
         return (0.0, 0.0)
 
     def fluid_pressure_initial_condition(self):
-        return "1.0 - 0.25*x[0]"
+        return "0.0"
 
     #--- Structure problem ---
 
@@ -75,7 +79,7 @@ class UpDown(FSI):
         return Structure()
 
     def structure_density(self):
-        return 10.0
+        return 0.1
 
     def structure_mu(self):
         return 5.0
@@ -84,17 +88,23 @@ class UpDown(FSI):
         return 0.25*125.0
 
     def structure_dirichlet_values(self):
-        return [(0.0, 0.0)]
+        return [Expression(("0.0","-2*A*x[1]*sin(pi*t)"),
+                           A=0.01, pi=DOLFIN_PI, t=0.0),
+                (0.0, 0.0),
+                Expression(("0.0","-2*A*x[1]*sin(pi*t)"),
+                           A=0.01, pi=DOLFIN_PI, t=0.0),
+                Expression(("0.0","-A*sin(pi*t)"),
+                           A=0.01, pi=DOLFIN_PI, t=0.0)]
 
     def structure_dirichlet_boundaries(self):
-        return [solid_bottom]
+        return [solid_left, solid_bottom, solid_right, fluid_bottom]
 
     def structure_neumann_boundaries(self):
         return [fluid_bottom]
 
     def structure_body_force(self):
         return Expression(("0.0", "2*pow(pi, 2.0)*A*rho_S*x[1]*sin(pi*t)"),
-                           pi=DOLFIN_PI, A=0.1, rho_S=self.structure_density(), t=0.0)
+                           pi=DOLFIN_PI, A=0.01, rho_S=self.structure_density(), t=0.0)
 
 
     #--- Parameters for mesh problem ---
