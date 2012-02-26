285a7ccefc2874d279b70c4041d4d7f66e81362b
Harish Narayanan <hnarayanan@gmail.com>
2012-02-26 13:38:41 +0100

    More work on the script to check/construct the new reference solution

--- demo/swing/reference2.py
+++ demo/swing/reference2.py
@@ -5,19 +5,20 @@ print("Structure problem in the reference configuration (S)")
 print("-"*72)
 
 # Declare useful symbols
-X, Y, t = symbols('x[0] x[1] t')
-A, rho_S, rho_F, mu, lam, eta = symbols('A rho_S rho_F mu lmbda eta')
+X, Y, x, y, t = symbols('X, Y, x, y, t')
+A, rho_S, rho_F, mu, lam, eta = symbols('A rho_S rho_F mu lambda eta')
 
 # Define a suitable solution field for the solid displacement
 U_S = Matrix([0.0,
-              A*Y*X*(1 - X)*sin(t)])
+              A*X*(1 - X)*sin(t)])
 print("U_S =\n%s" % U_S)
 
 # The kinematics of the motion are then described as follows
-I = Matrix([[1, 0], [0, 1]])
+I = eye(2)
 Grad_U_S = Matrix([[simplify(diff(U_S[0], X)), simplify(diff(U_S[0], Y))],
                    [simplify(diff(U_S[1], X)), simplify(diff(U_S[1], Y))]])
 F_S = I + Grad_U_S
+J_S = F_S.det()
 C = F_S.T*F_S
 E = Rational(1, 2)*(C - I)
 
@@ -33,7 +34,13 @@ print("B_S =\n%s" % B_S)
 
 # Check if (S) is satisfied
 check_S = rho_S*d2U_S_dt2 - Div_Sigma_S - B_S
-print("check_S =\n%s" % Matrix([simplify(check_S[0]), simplify(check_S[1])]))
+print("rho_S*d2U_S_dt2 - Div_Sigma_S - B_S =\n%s" %
+      Matrix([simplify(check_S[0]), simplify(check_S[1])]))
+
+# Compute the Cauchy stress tensor and check if it is symmetric
+sigma_S = (1/J_S)*Sigma_S*F_S.transpose()
+check_solid_stress = sigma_S - sigma_S.transpose()
+print("sigma_S - sigma_S^T =\n%s" % check_solid_stress)
 
 print("-"*72)
 print("Mesh problem in the reference configuration (M)")
@@ -41,72 +48,75 @@ print("-"*72)
 
 # Define a suitable solution field for the mesh displacement
 U_M = Matrix([0.0,
-              A*Y*X*(1 - X)*sin(t)])
+              2*A*(1 - Y)*X*(1 - X)*sin(t)])
 print("U_M =\n%s" % U_M)
 
 # Check whether U_S = U_M on the interface
 check_M = U_S.subs(Y, Rational(1, 2)) - U_M.subs(Y, Rational(1, 2))
-print("check_M =\n%s" % check_M)
+print("U_S(0, 1/2) - U_M(0, 1/2) =\n%s" % check_M)
 
-# Compute some kinematic quantities defined by U_M to be used in
-# pulling-back the Navier-Stokes equations to the reference fluid
-# configuration
-Grad_U_M = Matrix([[simplify(diff(U_M[0], X)), simplify(diff(U_M[0], Y))],
-                   [simplify(diff(U_M[1], X)), simplify(diff(U_M[1], Y))]])
-F_M = I + Grad_U_M
-J_M = F_M.det()
-F_M_inv = F_M.inv()
-V_M = Matrix([simplify(diff(U_M[0], t)),
-              simplify(diff(U_M[1], t))])
+print("-"*72)
+print("Fluid problem in the current configuration (f)")
+print("-"*72)
 
-# print("-"*72)
-# print("Fluid problem in the reference configuration (F)")
-# print("-"*72)
+# Define the solution field for the fluid velocity based on the time
+# derivative of the solid velocity on the interface to enforce the
+# no-slip condition
+V_F = Matrix([diff(U_S[0].subs(Y, Rational(1, 2)), t),
+              diff(U_S[1].subs(Y, Rational(1, 2)), t)])
 
-# # Define the solution field for the fluid velocity based on the time
-# # derivative of the solid velocity on the interface to enforce the
-# # no-slip condition
-# V_F = Matrix([diff(U_S[0].subs(Y, Rational(1, 2)), t),
-#               diff(U_S[1].subs(Y, Rational(1, 2)), t)])
-# print("V_F =\n%s" % V_F)
+# Cast the reference fluid coordinates in terms of the current fluid
+# coordinates
+X_x = solve(x - (X + U_M[0]), X)[0]
+Y_y = solve(y - (Y + U_M[1]), Y)[0]
 
-# # Check whether this functional form is divergence-free in the
-# # reference configuration
-# Div_term = J_M*F_M_inv*V_F
-# Div_V_F = simplify(diff(Div_term[0], X) + diff(Div_term[1], Y))
-# print("check_F_1 =\n%s" % Div_V_F)
+# Define the current fluid velocity
+v_f = Matrix([V_F[0].subs({Y:Y_y}).subs({X:X_x}),
+             V_F[1].subs({Y:Y_y}).subs({X:X_x})])
+print("v_f =\n%s" % v_f)
 
-# # Construct a part of the fluid stress, to determine the fluid
-# # pressure based on equality with the solid stress at the interface
-# P_F = symbols('P_F')
-# N_F = Matrix([0, -1.0])
-# Grad_V_F = Matrix([[simplify(diff(V_F[0], X)), simplify(diff(V_F[0], Y))],
-#                    [simplify(diff(V_F[1], X)), simplify(diff(V_F[1], Y))]])
+# Check whether this flow field is divergence free
+div_v_f = simplify(diff(v_f[0], x) + diff(v_f[1], y))
+print("div(v_f) =\n%s" % div_v_f)
 
-# Sigma_F = J_M*(eta*(Grad_V_F*F_M_inv + F_M_inv.T*Grad_V_F.T) - P_F*I)*F_M_inv.T
-# Sigma_F_int_dot_N = (Sigma_F*N_F).subs(Y, Rational(1, 2))
-# Sigma_S_int_dot_N = (Sigma_S*N_F).subs(Y, Rational(1, 2))
+# Find the current normal direction at the interface
+# FIXME: Check this super carefully
+U_FSI = U_M.subs({Y:0.5}).subs({X:x})
+n_f = Matrix([diff(U_FSI[1], x), -1.0]) / sqrt(diff(U_FSI[1], x)**2 + 1)
 
-# # Solve for the fluid pressure to satisfy the condition that the solid
-# # and fluid stresses are equal on the boundary
-# P_F_sol_1 = solve(Eq(Sigma_F_int_dot_N[0], Sigma_S_int_dot_N[0]), P_F)
-# print("P_F_1 =\n%s" % simplify(P_F_sol_1))
-# P_F_sol_2 = solve(Eq(Sigma_F_int_dot_N[1], Sigma_S_int_dot_N[1]), P_F)
-# print("P_F_2 =\n%s" % simplify(P_F_sol_2))
+# Construct a part of the fluid stress, to determine the fluid
+# pressure based on equality with the solid stress at the interface
+grad_v_f = Matrix([[simplify(diff(v_f[0], x)), simplify(diff(v_f[0], y))],
+                   [simplify(diff(v_f[1], x)), simplify(diff(v_f[1], y))]])
+p_f = symbols('p_f')
+sigma_f = eta*(grad_v_f + grad_v_f.T) - p_f*I
 
-# # Insert this pressure field into the definition of the fluid stress
-# Sigma_F = Sigma_F.subs(P_F, simplify(P_F_sol_2)[0])
-# print("check_F_2 =")
-# print(simplify(((Sigma_F*N_F - Sigma_S*N_F).subs(Y, Rational(1, 2)))[0]))
-# print(simplify(((Sigma_F*N_F - Sigma_S*N_F).subs(Y, Rational(1, 2)))[1]))
+sigma_f_int_dot_n = (sigma_f*n_f)
+sigma_s = sigma_S.subs({X:x})
+sigma_s_int_dot_n = (sigma_s*n_f)
 
-# # Construct terms of the Navier-Stokes equations starting with the
-# # time derivative of the velocity
-# dV_F_dt = Matrix([simplify(diff(V_F[0], t)), simplify(diff(V_F[1], t))])
+# Solve for the fluid pressure to satisfy the condition that the solid
+# and fluid stresses are equal on the boundary
+p_f_sol_1 = solve(Eq(sigma_f_int_dot_n[0], sigma_s_int_dot_n[0]), p_f)[0]
+p_f_sol_2 = solve(Eq(sigma_f_int_dot_n[1], sigma_s_int_dot_n[1]), p_f)[0]
 
-# # Divergence of the stress field
-# Div_Sigma_F = Matrix([simplify(diff(Sigma_F[0], X) + diff(Sigma_F[1], Y)),
-#                       simplify(diff(Sigma_F[2], X) + diff(Sigma_F[3], Y))])
+# Insert one of these pressure fields into the definition of the fluid
+# stress
+print("p_f =\n%s" % simplify(p_f_sol_1))
+sigma_f = sigma_f.subs(p_f, simplify(p_f_sol_1))
+
+
+# print("check_f_2 =")
+# print(simplify(((sigma_f*n_f - sigma_s*n_f).subs(Y, Rational(1, 2)))[0]))
+# print(simplify(((sigma_f*n_f - sigma_s*n_f).subs(Y, Rational(1, 2)))[1]))
+
+# Construct terms of the Navier-Stokes equations starting with the
+# time derivative of the velocity
+dv_f_dt = Matrix([simplify(diff(v_f[0], t)), simplify(diff(v_f[1], t))])
+
+# divergence of the stress field
+div_sigma_f = Matrix([simplify(diff(sigma_f[0], x) + diff(sigma_f[1], y)),
+                      simplify(diff(sigma_f[2], x) + diff(sigma_f[3], y))])
 
 # # Use (F) to determine B_F
 # B_F = rho_F*J_M*(dV_F_dt + Grad_V_F*F_M_inv*(V_F - V_M)) - Div_Sigma_F
