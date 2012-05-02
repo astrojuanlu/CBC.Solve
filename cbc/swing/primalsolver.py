68db65007cf1cd64e3137cf4d4dae3ee3c677e4a
Marie E. Rognes <meg@simula.no>
2012-05-02 14:46:47 +0200

    Remove a backslash

--- cbc/swing/primalsolver.py
+++ cbc/swing/primalsolver.py
@@ -1,10 +1,10 @@
-\"This module implements the primal FSI solver."
+"This module implements the primal FSI solver."
 
 __author__ = "Kristoffer Selim and Anders Logg"
 __copyright__ = "Copyright (C) 2010 Simula Research Laboratory and %s" % __author__
 __license__  = "GNU GPL Version 3 or any later version"
 
-# Last changed: 2012-04-18
+# Last changed: 2012-05-02
 
 import pylab
 from time import time as python_time
