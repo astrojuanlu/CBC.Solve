7885bc8e0a62f658f7d06ce66da19716ff5e0b99
Anders Logg <logg@glaurung>
2012-05-03 12:33:26 +0200

    Small fixes

--- demo/swing/analytic/run_convergence_test_primal.py
+++ demo/swing/analytic/run_convergence_test_primal.py
@@ -7,7 +7,7 @@ __copyright__ = "Copyright (C) 2012 Simula Research Laboratory and %s" % __autho
 __license__  = "GNU GPL Version 3 or any later version"
 
 # First added:  2012-03-05
-# Last changed: 2012-05-02
+# Last changed: 2012-05-03
 
 from cbc.swing import *
 
@@ -25,4 +25,4 @@ p["fixedpoint_tolerance"] = 1e-14
 p["max_num_refinements"] = 100
 
 # Run
-run_local("analytic", p, "convergence_test_primal")
+run_local("analytic", p, "convergence-test-primal")
